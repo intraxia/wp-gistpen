115443977696f20d334905f5ecb7b4bf208717e6
mAAdhaTTah <jamesorodig@gmail.com>
2014-08-02 23:12:24 -0400

    Beginning of option page
    * Create option page
    * Select syntax highlighting theme
    * On-page demo

--- admin/assets/js/wp-gistpen-admin.js
+++ admin/assets/js/wp-gistpen-admin.js
@@ -1,10 +1,25 @@
-(function ( $ ) {
+(function () {
 	"use strict";
 
-	$(function () {
+	jQuery(function () {
 
-		// Place your administration-specific JavaScript here
+		var theme = jQuery( '#_wpgp_gistpen_highlighter_theme' ).val();
+		if( theme == 'default' ) {
+			theme = '';
+		} else {
+			theme = '-' + theme;
+		}
+		jQuery( 'link#wp-gistpen-prism-style-theme-css' ).attr( 'href', WP_GISTPEN_URL + 'public/assets/vendor/prism/themes/prism' + theme + '.css'  );
 
+		jQuery( '#_wpgp_gistpen_highlighter_theme' ).change( function() {
+			var theme = jQuery( '#_wpgp_gistpen_highlighter_theme' ).val();
+			if( theme == 'default' ) {
+				theme = '';
+			} else {
+				theme = '-' + theme;
+			}
+			jQuery( '#wp-gistpen-prism-style-theme-css' ).attr( 'href', WP_GISTPEN_URL + 'public/assets/vendor/prism/themes/prism' + theme + '.css'  );
+		});
 	});
 
 }(jQuery));
