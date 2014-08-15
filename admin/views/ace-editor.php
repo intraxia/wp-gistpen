5638c391a6aca983d6d4073c6029dd0b5addd065
mAAdhaTTah <jamesorodig@gmail.com>
2014-08-14 23:46:01 -0400

    Improved editor switching
    Unfortunately, no text syncing yet.

--- admin/views/ace-editor.php
+++ admin/views/ace-editor.php
@@ -11,7 +11,7 @@
 ?>
 
 <style type="text/css" media="screen">
-	#wp-content-wrap {
+	#ace-editor {
 		min-height: 360px;
 		width: 100%;
 	}
@@ -19,12 +19,66 @@
 
 <script src="<?php echo WP_GISTPEN_URL; ?>public/assets/vendor/ace-builds/src-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
 <script>
-	var aceeditor;
 
-	jQuery(function() {
-		aceeditor = ace.edit('wp-content-wrap');
-		aceeditor.setTheme("ace/theme/monokai");
-		aceeditor.getSession().setMode("ace/mode/javascript");
-		aceeditor.setValue("<?php global $post; echo esc_textarea($post->post_content); ?>");
-	});
+	jQuery(function() { Ace.init(); });
+
+	var Ace = {
+
+		init: function() {
+			this.aceEditorId = 'ace-editor';
+			this.aceEditorDiv = jQuery('<div id="' + this.aceEditorId + '"></div>').css({
+				left: 0,
+				top: 0,
+				bottom: 0,
+				right: 0,
+				zIndex: 1
+			});
+			this.textButton = jQuery('<a id="content-html" class="hide-if-no-js wp-switch-editor switch-html">Text</a>');
+			this.aceButton = jQuery('<a id="content-ace" class="hide-if-no-js wp-switch-editor switch-ace">Ace</a>');
+
+			this.addSwitchButtons();
+			this.loadClickHandlers();
+			this.activateAceEditor();
+		},
+
+		addSwitchButtons: function() {
+			this.textButton.appendTo('#wp-content-editor-tools');
+			this.aceButton.appendTo('#wp-content-editor-tools');
+		},
+
+		loadClickHandlers: function() {
+			this.textButton.click(function() {
+				Ace.switchToText();
+			}
+			);
+			this.aceButton.click(function() {
+				Ace.switchToAce();
+			});
+		},
+
+		switchToText: function() {
+			jQuery('#' + this.aceEditorId).hide();
+			jQuery('#wp-content-wrap').addClass('html-active').removeClass('ace-active');
+			jQuery('#content').show();
+		},
+
+		switchToAce: function() {
+			jQuery('#' + this.aceEditorId).show();
+			jQuery('#wp-content-wrap').removeClass('html-active').addClass('ace-active');
+			jQuery('#content').hide();
+		},
+
+		activateAceEditor: function() {
+			this.aceEditorDiv.insertAfter('#content');
+
+			// Set up editor on div
+			aceEditor = ace.edit(this.aceEditorId);
+			aceEditor.setTheme('ace/theme/monokai');
+			aceEditor.getSession().setMode('ace/mode/javascript');
+			aceEditor.setValue(jQuery('#content').val());
+
+			this.switchToAce();
+			jQuery('#wp-content-media-buttons, #ed_toolbar').hide();
+		}
+	};
 </script>
