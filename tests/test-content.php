872e961b54cd75a58f259af6976a92f75037cede
mAAdhaTTah <jamesorodig@gmail.com>
2014-09-07 15:58:15 -0400

    Write tests for content

---
+++ tests/test-content.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * @group content
+ */
+class WP_Gistpen_Content_Test extends WP_UnitTestCase {
+
+	public $gistpen;
+	public $gistpenfiles;
+
+	function setUp() {
+		parent::setUp();
+
+		$this->factory = new WP_UnitTest_Factory();
+
+		$this->gistpen = $this->factory->post->create_and_get( array(
+			'post_type' => 'gistpen',
+		), array(
+			'post_title' => new WP_UnitTest_Generator_Sequence( 'Post title %s' ),
+			'post_name' => new WP_UnitTest_Generator_Sequence( 'Post title %s' )
+		));
+
+		$this->gistpenfiles = $this->factory->post->create_many( 3, array(
+			'post_type' => 'gistpen',
+		), array(
+			'post_title' => new WP_UnitTest_Generator_Sequence( 'Post title %s' ),
+			'post_name' => new WP_UnitTest_Generator_Sequence( 'Post title %s' ),
+			'post_content' => new WP_UnitTest_Generator_Sequence( 'Post content %s' )
+		));
+		foreach ( $this->gistpenfiles as $gistpenfile_id ) {
+			$this->factory->post->update_object( $gistpenfile_id, array( 'post_parent' => $this->gistpen->ID ) );
+		}
+	}
+
+	function test_get_post_content() {
+		$content = WP_Gistpen_Content::get_post_content( $this->gistpen );
+
+		$sub_str_count = substr_count( $content, '<h2 class="wp-gistpenfile-title">' );
+		$this->assertEquals( 3, $sub_str_count );
+	}
+
+	function test_get_shortcode_content_child() {
+		$content = WP_Gistpen_Content::get_shortcode_content( array( 'id' => $this->gistpenfiles[0], 'highlight' => null ) );
+
+		$sub_str_count = substr_count( $content, '<h2 class="wp-gistpenfile-title">' );
+		$this->assertEquals( 1, $sub_str_count );
+		$sub_str_count = substr_count( $content, 'data-line=' );
+		$this->assertEquals( 0, $sub_str_count );
+	}
+
+	function test_get_shortcode_content_parent() {
+		$content = WP_Gistpen_Content::get_shortcode_content( array( 'id' => $this->gistpen->ID, 'highlight' => null ) );
+
+		$sub_str_count = substr_count( $content, '<h2 class="wp-gistpenfile-title">' );
+		$this->assertEquals( 3, $sub_str_count );
+		$sub_str_count = substr_count( $content, 'data-line=' );
+		$this->assertEquals( 0, $sub_str_count );
+	}
+
+	function test_get_shortcode_with_highlight() {
+		$content = WP_Gistpen_Content::get_shortcode_content( array( 'id' => $this->gistpenfiles[0], 'highlight' => '1' ) );
+
+		$sub_str_count = substr_count( $content, 'data-line=' );
+		$this->assertEquals( 1, $sub_str_count );
+	}
+
+	function tearDown() {
+		parent::tearDown();
+	}
+}
