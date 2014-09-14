db53ff965f7163fd512bc13a33647d85374a8b10
mAAdhaTTah <jamesorodig@gmail.com>
2014-09-13 21:32:15 -0400

    Fix broken object tests

--- test/tests/test-language.php
+++ test/tests/test-language.php
@@ -5,8 +5,6 @@
  */
 class WP_Gistpen_Language_Test extends WP_Gistpen_UnitTestCase {
 
-	public $mock_term;
-
 	public $language;
 
 	function setUp() {
@@ -14,78 +12,57 @@ class WP_Gistpen_Language_Test extends WP_Gistpen_UnitTestCase {
 	}
 
 	function test_return_prism_slug() {
-		$this->mock_lang
-			->expects( $this->any() )
-			->method( '__get' )
-			->with( 'slug' )
-			->will( $this->returnValue('slug') );
-		$this->language = new WP_Gistpen_Language( $this->mock_lang );
+		$term = new stdClass;
+		$term->slug = 'slug';
+		$this->language = new WP_Gistpen_Language( $term );
 
 		$this->assertEquals( 'slug', $this->language->prism_slug );
 	}
 
 	function test_fix_prism_slug_javascript() {
-		$this->mock_lang
-			->expects( $this->any() )
-			->method( '__get' )
-			->with( 'slug' )
-			->will( $this->returnValue('js') );
-		$this->language = new WP_Gistpen_Language( $this->mock_lang );
+		$term = new stdClass;
+		$term->slug = 'js';
+		$this->language = new WP_Gistpen_Language( $term );
 
 		$this->assertEquals( 'javascript', $this->language->prism_slug );
 	}
 
 	function test_fix_prism_slug_sass() {
-		$this->mock_lang
-			->expects( $this->any() )
-			->method( '__get' )
-			->with( 'slug' )
-			->will( $this->returnValue('sass') );
-		$this->language = new WP_Gistpen_Language( $this->mock_lang );
+		$term = new stdClass;
+		$term->slug = 'sass';
+		$this->language = new WP_Gistpen_Language( $term );
 
 		$this->assertEquals( 'scss', $this->language->prism_slug );
 	}
 
 	function test_return_file_ext() {
-		$this->mock_lang
-			->expects( $this->any() )
-			->method( '__get' )
-			->with( 'slug' )
-			->will( $this->returnValue('slug') );
-		$this->language = new WP_Gistpen_Language( $this->mock_lang );
+		$term = new stdClass;
+		$term->slug = 'slug';
+		$this->language = new WP_Gistpen_Language( $term );
 
 		$this->assertEquals( 'slug', $this->language->file_ext );
 	}
 
 	function test_fix_file_ext_bash() {
-		$this->mock_lang
-			->expects( $this->any() )
-			->method( '__get' )
-			->with( 'slug' )
-			->will( $this->returnValue('bash') );
-		$this->language = new WP_Gistpen_Language( $this->mock_lang );
+		$term = new stdClass;
+		$term->slug = 'bash';
+		$this->language = new WP_Gistpen_Language( $term );
 
 		$this->assertEquals( 'sh', $this->language->file_ext );
 	}
 
 	function test_fix_file_ext_sass() {
-		$this->mock_lang
-			->expects( $this->any() )
-			->method( '__get' )
-			->with( 'slug' )
-			->will( $this->returnValue('sass') );
-		$this->language = new WP_Gistpen_Language( $this->mock_lang );
+		$term = new stdClass;
+		$term->slug = 'sass';
+		$this->language = new WP_Gistpen_Language( $term );
 
 		$this->assertEquals( 'scss', $this->language->file_ext );
 	}
 
 	function test_return_display_name() {
-		$this->mock_lang
-			->expects( $this->any() )
-			->method( '__get' )
-			->with( 'name' )
-			->will( $this->returnValue('Language name') );
-		$this->language = new WP_Gistpen_Language( $this->mock_lang );
+		$term = new stdClass;
+		$term->name = 'Language name';
+		$this->language = new WP_Gistpen_Language( $term );
 
 		$this->assertEquals( 'Language name', $this->language->display_name );
 	}
