854d34602922647742e119d6b943bdcc42a34457
mAAdhaTTah <jamesorodig@gmail.com>
2014-11-27 14:56:18 -0500

    Scaffold test data

---
+++ app/CLI/Command.php
@@ -0,0 +1,26 @@
+<?php
+namespace WP_Gistpen\CLI;
+
+use \WP_CLI;
+
+/**
+ * Registers the CLI commands for WP-Gistpen
+ *
+ * @package    WP_Gistpen
+ * @author     James DiGioia <jamesorodig@gmail.com>
+ * @link       http://jamesdigioia.com/wp-gistpen/
+ * @since      0.5.0
+ */
+class Command extends \WP_CLI_Command {
+
+	/**
+	 * Adds the test Gistpen data.
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     wp wpgp add_test_data
+	 */
+	function add_test_data( $args, $assoc_args ) {
+
+	}
+}
