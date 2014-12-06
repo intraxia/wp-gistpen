4686bbc97b2b405a3a75a0fcdae40bdeb3fc84dd
mAAdhaTTah <jamesorodig@gmail.com>
2014-12-06 13:20:18 -0500

    Add new add_error function to save_post hooks

--- app/Register/Save.php
+++ app/Register/Save.php
@@ -141,9 +141,8 @@ class Save {
 		remove_action( 'save_post', array( $this, 'save_post_hook' ), 10, 1 );
 
 		$result = $this->database->persist()->by_zip( $zip );
-		if ( is_wp_error( $result ) ) {
-			$this->errors .= $result->get_error_code() . ',';
-		}
+
+		$this->add_error( $result );
 
 		add_action( 'save_post', array( $this, 'save_post_hook' ), 10, 1 );
 	}
@@ -199,48 +198,6 @@ class Save {
 	}
 
 	/**
-	 * Retrieves, validates, and organizes the arguments for the File
-	 * from the $_POST superglobal
-	 *
-	 * @param  string $file_id_w_dash
-	 * @return array                 Arguments required for manipulating File model object
-	 * @since  0.5.0
-	 */
-	private function get_args( $file_id ) {
-		$args = array();
-
-		// @todo validation
-		$args['slug'] = $_POST[ 'wp-gistpenfile-slug-' . $file_id ];
-		$args['code'] = $_POST[ 'wp-gistpenfile-code-' . $file_id ];
-		$args['language'] = $this->adapter->build( 'language' )->by_slug( $_POST[ 'wp-gistpenfile-language-' . $file_id ] );
-
-		return $args;
-	}
-
-	/**
-	 * Check if we need to add errors to the rediect
-	 * and add the filter if we do
-	 *
-	 * @since 0.5.0
-	 */
-	public function check_errors() {
-		if ( $this->errors !== '' ) {
-			add_filter( 'redirect_post_location',array( $this, 'return_errors' ) );
-		}
-	}
-
-	/**
-	 * Adds the errors to the url, if any
-	 *
-	 * @param  string $location Current GET params
-	 * @return string           Updated GET params
-	 * @since  0.5.0
-	 */
-	public function return_errors( $location ) {
-		return add_query_arg( 'gistpen-errors', rtrim( $this->errors, ',' ), $location );
-	}
-
-	/**
 	 * Keeps the File's post_status in sync with
 	 * the Zip's post_status
 	 *
@@ -257,20 +214,18 @@ class Save {
 			$files = $this->database->query()->files_by_post( $post );
 
 			foreach ( $files as $file ) {
-				remove_action( 'save_post_gistpen', array( $this, 'save_post_hook' ) );
-				remove_action( 'transition_post_status', array( $this, 'sync_post_status' ) );
+				remove_action( 'save_post_gistpen', array( $this, 'save_post_hook' ), 10, 1 );
+				remove_action( 'transition_post_status', array( $this, 'sync_post_status', 10, 3 ) );
 
 				$result = wp_update_post( array(
 					'ID' => $file->get_ID(),
 					'post_status' => $new_status,
 				), true );
 
-				add_action( 'save_post_gistpen', array( $this, 'save_post_hook' ) );
+				add_action( 'save_post_gistpen', array( $this, 'save_post_hook' ), 10, 1 );
 				add_action( 'transition_post_status', array( $this, 'sync_post_status' ), 10, 3 );
 
-				if ( is_wp_error( $result ) ) {
-					$this->errors .= $result->get_error_code() . ',';
-				}
+				$this->add_error( $result );
 			}
 		}
 	}
@@ -295,12 +250,58 @@ class Save {
 				$result = wp_delete_post( $file->get_ID(), true );
 				add_action( 'delete_post', array( $this, 'delete_post_hook' ) );
 
-				if ( is_wp_error( $result ) ) {
-					$this->errors .= $result->get_error_code() . ',';
-				}
+				$this->add_error( $result );
 			}
 
 			$this->check_errors();
 		}
+	}
+
+	/**
+	 * Retrieves, validates, and organizes the arguments for the File
+	 * from the $_POST superglobal
+	 *
+	 * @param  string $file_id_w_dash
+	 * @return array                 Arguments required for manipulating File model object
+	 * @since  0.5.0
+	 */
+	private function get_args( $file_id ) {
+		$args = array();
+
+		// @todo validation
+		$args['slug'] = $_POST[ 'wp-gistpenfile-slug-' . $file_id ];
+		$args['code'] = $_POST[ 'wp-gistpenfile-code-' . $file_id ];
+		$args['language'] = $this->adapter->build( 'language' )->by_slug( $_POST[ 'wp-gistpenfile-language-' . $file_id ] );
+
+		return $args;
+	}
+
+	public function add_error( $result ) {
+		if ( is_wp_error( $result ) ) {
+			$this->errors .= $result->get_error_code() . ',';
+		}
+	}
+
+	/**
+	 * Check if we need to add errors to the rediect
+	 * and add the filter if we do
+	 *
+	 * @since 0.5.0
+	 */
+	public function check_errors() {
+		if ( $this->errors !== '' ) {
+			add_filter( 'redirect_post_location',array( $this, 'return_errors' ) );
+		}
+	}
+
+	/**
+	 * Adds the errors to the url, if any
+	 *
+	 * @param  string $location Current GET params
+	 * @return string           Updated GET params
+	 * @since  0.5.0
+	 */
+	public function return_errors( $location ) {
+		return add_query_arg( 'gistpen-errors', rtrim( $this->errors, ',' ), $location );
 	}
 }
