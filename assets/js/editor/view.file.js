6b73c6581b5587ad4c7aee6a3bf0b97b60cc54d0
mAAdhaTTah <jamesorodig@gmail.com>
2015-01-11 10:09:01 -0500

    Fix ACE for Coffeescript

--- assets/js/editor/view.file.js
+++ assets/js/editor/view.file.js
@@ -86,6 +86,8 @@
 				this.aceEditor.getSession().setMode('ace/mode/sh');
 			} else if('c' === modelLang || 'cpp' ===modelLang) {
 				this.aceEditor.getSession().setMode('ace/mode/c_cpp');
+			} else if('coffeescript' === modelLang) {
+				this.aceEditor.getSession().setMode('ace/mode/coffee');
 			} else {
 				this.aceEditor.getSession().setMode('ace/mode/' + modelLang);
 			}
