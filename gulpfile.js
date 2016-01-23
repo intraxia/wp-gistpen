eda0f3256156357f9ad6d37c2cf4004d8b899ba8
mAAdhaTTah <jamesorodig@gmail.com>
2016-01-23 12:15:53 -0500

    Remove bower dependency and load ace from npm
    Simplifies our dependency management overall.

--- gulpfile.js
+++ gulpfile.js
@@ -62,7 +62,7 @@ gulp.task('styles', function () {
 });
 
 gulp.task('ace', function () {
-    return gulp.src('bower_components/ace-builds/src-min-noconflict/**')
+    return gulp.src('node_modules/ace-builds/src-min-noconflict/**')
         .pipe(gulp.dest('assets/js/ace'));
 });
 
