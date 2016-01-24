48f46d7bcb3520b14a5652951a9125de977fde7b
mAAdhaTTah <jamesorodig@gmail.com>
2016-01-24 16:24:57 -0500

    Create and wire up the copy-to-clipboard button
    Also makes the Prism wrapper inherit from it, so we don't
    have to write wrapping methods.

--- src/js/web.js
+++ src/js/web.js
@@ -1,5 +1,6 @@
 var Prism = require('./prism');
 var Plite = require('plite');
+var toolbar = require('./prism/toolbar');
 
 var promises = [];
 promises.push(Prism.loadTheme(Gistpen_Settings.prism.theme));
@@ -8,6 +9,10 @@ promises.push(Prism.loadTheme(Gistpen_Settings.prism.theme));
 if (Gistpen_Settings.prism.plugins['line-numbers'].enabled) {
     promises.push(Prism.loadPlugin('line-numbers'));
 }
+
+promises.push(Prism.loadPlugin('toolbar'));
+
+Prism.hooks.add('after-highlight', toolbar.hook);
 
 Plite.all(promises)
     .then(Prism.highlightAll)
