18e77e373418297246f5de58aa84f54bb878d568
mAAdhaTTah <jamesorodig@gmail.com>
2016-01-24 22:36:38 -0500

    Extract copy to clipboard button to module

---
+++ src/js/prism/clipboard.js
@@ -0,0 +1,31 @@
+var Clipboard = require('clipboard');
+
+exports.button = function button(env) {
+    var linkCopy = document.createElement('a');
+    linkCopy.innerHTML = 'Copy';
+
+    var clip = new Clipboard(linkCopy, {
+        'text': function () {
+            return env.code;
+        }
+    });
+
+    clip.on('success', function() {
+        linkCopy.innerHTML = 'Copied!';
+
+        resetText();
+    });
+    clip.on('error', function () {
+        linkCopy.innerHTML = 'Press Ctrl+C to copy';
+
+        resetText();
+    });
+
+    return linkCopy;
+
+    function resetText() {
+        setTimeout(function () {
+            linkCopy.innerHTML = 'Copy';
+        }, 7000);
+    }
+};
