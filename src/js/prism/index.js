23c389305b2a6c1ceedfafd5872e287291971097
mAAdhaTTah <jamesorodig@gmail.com>
2016-01-30 14:52:38 -0500

    Add theme and plugin async loaders
    Yay promises!
    
    Related #49

--- src/js/prism/index.js
+++ src/js/prism/index.js
@@ -1,42 +1,20 @@
-var Plite = require('plite');
 var Prism = require('./compiled');
 var extend = require('lodash.assign');
 
-var css = document.createElement("link");
-css.type = "text/css";
-css.rel = "stylesheet";
-document.getElementsByTagName("head")[0].appendChild(css);
 
-var api = {
-    loadTheme: loadTheme,
-    loadPlugin: loadPlugin
-};
 
-extend(api, Prism);
+
+
+
+
+var api = {};
+
+/**
+ * Add our extensions.
+ */
+var themeloader = require('./themeloader');
+var pluginloader = require('./pluginloader');
+
+extend(api, Prism, themeloader, pluginloader);
+
 module.exports = api;
-
-
-function loadTheme(theme) {
-    return Plite(function(resolve, reject) {
-        theme = theme === 'default' ? '' : '-' + theme;
-
-        css.onload = resolve;
-        css.onerror = reject;
-
-        css.href = Gistpen_Settings.url + 'assets/css/prism' + theme + '.css';
-    });
-}
-
-function loadPlugin(plugin) {
-    return Plite(function(resolve, reject) {
-        var css = document.createElement("link");
-        css.type = "text/css";
-        css.rel = "stylesheet";
-
-        css.onload = resolve;
-        css.onerror = reject;
-
-        css.href = Gistpen_Settings.url + 'assets/css/prism-' + plugin + '.css';
-        document.getElementsByTagName("head")[0].appendChild(css);
-    });
-}
