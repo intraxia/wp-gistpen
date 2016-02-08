7368ba01e56254f30d1303baa322c8467c92d578
mAAdhaTTah <jamesorodig@gmail.com>
2016-02-07 19:11:32 -0500

    Create dispatches for wiring actions
    This is where we can provide the seam to wire up the AJAX
    calls and update the UI accordingly.

---
+++ src/js/admin/dispatches.js
@@ -0,0 +1,60 @@
+import fetchival from 'fetchival';
+import { polyfill } from 'es6-promise';
+import { setAjaxStatus, AJAX } from '../wordpress/actions';
+import {
+    updatePrismTheme,
+    toggleLineNumbers,
+    toggleShowInvisibles,
+    resetSiteState
+} from './actions';
+import { dispatch } from './store';
+
+if (!window.Promise) {
+    polyfill();
+}
+
+const api = fetchival(`${window.Gistpen_Settings.root}site`, {
+    credentials: 'include',
+    headers: {
+        'X-WP-Nonce': window.Gistpen_Settings.nonce
+    }
+});
+
+export function handlePrismThemeChange(event) {
+    return dispatch(
+        updatePrismTheme(event.target.value)
+    );
+}
+
+export function handleLineNumbersChange(event) {
+    return dispatch(
+        toggleLineNumbers(event.target.checked)
+    );
+}
+
+export function handleShowInvisiblesChange(event) {
+    return dispatch(
+        toggleShowInvisibles(event.target.checked)
+    );
+}
+
+let timeout;
+
+export function handleServerUpdate(patch) {
+    if (timeout) {
+        clearTimeout(timeout);
+    }
+
+    dispatch(setAjaxStatus(AJAX.UPDATING));
+
+    api.patch(patch)
+        // Update our site state from the server
+        .then((site) => dispatch(resetSiteState(site)))
+        // Notify the UI that update is complete
+        .then(() => dispatch(setAjaxStatus(AJAX.SUCCESS)))
+        // Error handling
+        .catch(() => dispatch(setAjaxStatus(AJAX.ERROR)))
+        // Wait then reset error message
+        .then(() => new Promise((resolve) => timeout = setTimeout(resolve, 5000)))
+        .then(() => dispatch(setAjaxStatus(AJAX.IDLE)));
+}
