9abae7b62f37be5881ee1196319d2dde55c8dd37
mAAdhaTTah <jamesorodig@gmail.com>
2016-02-07 21:49:41 -0500

    Add Text component to wp pattern lib

--- src/js/wordpress/index.js
+++ src/js/wordpress/index.js
@@ -4,3 +4,4 @@ export { default as Container } from './container';
 export { default as Dropdown } from './dropdown';
 export { default as Header } from './header';
 export { default as Status } from './status';
+export { default as Text } from './text';
