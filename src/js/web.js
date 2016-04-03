1fa94adae0f6006ff688ed5da3625a086fc5d7a6
mAAdhaTTah <jamesorodig@gmail.com>
2016-04-03 11:25:52 -0400

    Load Prism on DOMContentLoaded
    Previously, the script could execute before the body was
    fully loaded, which meant that injected scripts failed to be
    properly injected, since the element wasn't ready to have
    children injected into it.

--- src/js/web.js
+++ src/js/web.js
@@ -7,37 +7,31 @@ if (!window.Promise) {
     require('es6-promise').polyfill();
 }
 
-/**
- * Configure Prism.
- */
-Prism.plugins.autoloader.languages_path = Gistpen_Settings.url + 'assets/js/';
-Prism.setDebug("1" === Gistpen_Settings.debug);
+document.addEventListener('DOMContentLoaded', () => {
+    /**
+     * Configure Prism.
+     */
+    Prism.plugins.autoloader.languages_path = Gistpen_Settings.url + 'assets/js/';
+    Prism.setDebug("1" === Gistpen_Settings.debug);
 
-/**
- * Begin loading out dependencies.
- */
+    /**
+     * Begin loading out dependencies.
+     */
 
-const promises = [];
+    const promises = [];
 
-promises.push(Prism.loadTheme(Gistpen_Settings.site.prism.theme));
-promises.push(Prism.loadCSS('toolbar'));
+    promises.push(Prism.loadTheme(Gistpen_Settings.site.prism.theme));
+    promises.push(Prism.loadCSS('toolbar'));
 
-if (Gistpen_Settings.site.prism['line-numbers']) {
-    promises.push(Prism.loadPlugin('line-numbers'));
-}
+    if (Gistpen_Settings.site.prism['line-numbers']) {
+        promises.push(Prism.loadPlugin('line-numbers'));
+    }
 
-if (Gistpen_Settings.site.prism['show-invisibles']) {
-    promises.push(Prism.loadPlugin('show-invisibles'));
-}
+    if (Gistpen_Settings.site.prism['show-invisibles']) {
+        promises.push(Prism.loadPlugin('show-invisibles'));
+    }
 
-const chain = window.PrismPromise = Promise.all(promises);
-const finish = () => chain
-        .then(Prism.highlightAll)
-        .catch(console.error.bind(console));
-
-if (document.readyState === "complete" || document.readyState === "loaded") {
-    finish();
-} else {
-    document.addEventListener('DOMContentLoaded', finish);
-}
-
+    window.PrismPromise = Promise.all(promises)
+            .then(Prism.highlightAll)
+            .catch(console.error.bind(console));
+});
