3b033115f881a87bc5304ea1ede228f2879be33d
mAAdhaTTah <jamesorodig@gmail.com>
2016-09-12 21:39:06 -0400

    Update the state in response to route changes
    Mount the reducer to the store.

--- client/reducer/index.js
+++ client/reducer/index.js
@@ -1,11 +1,12 @@
 import R from 'ramda';
 import { combineReducers } from 'redux';
+import route from './route';
 
 const defaultReducer = R.pipe(R.defaultTo({}), R.identity);
 
 export default combineReducers({
     const: defaultReducer,
-    route: defaultReducer,
+    route,
     prism: defaultReducer,
     gist: defaultReducer
 });
