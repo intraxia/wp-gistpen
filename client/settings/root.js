6e5ece6f77c4365408391a548e171f6ffc57f5c3
mAAdhaTTah <jamesorodig@gmail.com>
2016-09-13 08:25:32 -0400

    Wire up highlighting settings screen
    Bump commit hash to get onchange support.

--- client/settings/root.js
+++ client/settings/root.js
@@ -2,11 +2,13 @@ import component from 'brookjs/component';
 import render from 'brookjs/render';
 import children from 'brookjs/children';
 import header from './header';
+import highlighting from './highlighting';
 import template from './index.hbs';
 
 export default component({
     render: render(template),
     children: children({
-        settingsHeader: header
+        settingsHeader: header,
+        settingsHighlighting: highlighting
     })
 });
