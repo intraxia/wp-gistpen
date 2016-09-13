006533f77262c01221825405f8740d55e9465fda
mAAdhaTTah <jamesorodig@gmail.com>
2016-09-12 22:29:15 -0400

    Add header component and update on route
    The `settingsHeader` is bound up and updates the active header
    tab's class.

--- client/settings/root.js
+++ client/settings/root.js
@@ -1,7 +1,12 @@
 import component from 'brookjs/component';
 import render from 'brookjs/render';
+import children from 'brookjs/children';
+import header from './header';
 import template from './index.hbs';
 
 export default component({
-    render: render(template)
+    render: render(template),
+    children: children({
+        settingsHeader: header
+    })
 });
