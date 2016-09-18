5cab80228455270c6ed162eb2888f641f678325f
mAAdhaTTah <jamesorodig@gmail.com>
2016-09-18 12:41:33 -0400

    Add missing docblocks in JS

--- client/action/router.js
+++ client/action/router.js
@@ -1,5 +1,17 @@
+/**
+ * Dispatched when route changes.
+ *
+ * @type {string}
+ */
 export const ROUTE_CHANGE = 'ROUTE_CHANGE';
 
+/**
+ * Create a new Route change action.
+ *
+ * @param {string} route - Route changed to.
+ * @param {Object} [parts] - Additional route parts.
+ * @returns {Action} Route change action.
+ */
 export function routeChangeAction(route, parts = {}) {
     return {
         type: ROUTE_CHANGE,
--- client/helpers/compare.js
+++ client/helpers/compare.js
@@ -1,3 +1,11 @@
+/**
+ * Handlebars helper to compare two values.
+ *
+ * @param {*} first - First value
+ * @param {*} second - Second value.
+ * @param {Object} options - Handlebars options.
+ * @returns {string} Template chunk.
+ */
 module.exports = function compare(first, second, options) {
     if (first === second) {
         return options.fn(this);
