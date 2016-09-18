529384bbb32e21e569297ed697f88e1893d1039c
mAAdhaTTah <jamesorodig@gmail.com>
2016-09-18 15:11:11 -0400

    Delete dead code & unneeded dependencies
    Remove code from previous implementation.

--- src/js/wordpress/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Checkbox } from './checkbox';
-export { default as Container } from './container';
-export { default as Dropdown } from './dropdown';
-export { default as Header } from './header';
-export { default as Status } from './status';
-export { default as Text } from './text';
