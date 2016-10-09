0a3c00d1a20e2d1a4d2d89c9a0398e5da004c310
mAAdhaTTah <jamesorodig@gmail.com>
2016-10-09 15:43:17 -0400

    Expand SettingsState to cover all properties
    Fuller typechecking on the state.

--- client/type/state.js
+++ client/type/state.js
@@ -5,6 +5,27 @@ export type PrismState = {
     'show-invisibles' : boolean;
 };
 
+export type GistState = {
+    token : string;
+};
+
+export type RouteState  = 'highlighting' | 'accounts' | 'import' | 'export';
+
+export type ConstState = {
+    languages : { [key : string] : string };
+    root : string;
+    nonce : string;
+    url : string;
+    ace_themes : { [key : string] : string };
+    ace_widths : Array<number>;
+    statuses : { [key : string] : string };
+    themes : { [key : string] : string };
+    repo? : Object;
+};
+
 export type SettingsState = {
     prism : PrismState;
+    gist : GistState;
+    route : RouteState;
+    'const' : ConstState;
 };
