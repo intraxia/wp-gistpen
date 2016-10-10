2d37d605d3d2bfc3d4e301eb0fb8ea0333b62f74
mAAdhaTTah <jamesorodig@gmail.com>
2016-10-10 16:44:38 -0400

    Rename const -> globals
    Using `const` was stupid, since its a reserved word in JS.
    Rename to `globals` before we go further.

--- client/delta/index.js
+++ client/delta/index.js
@@ -1,8 +1,9 @@
 import R from 'ramda';
 import { applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { observeDelta } from 'brookjs';
 
 export { default as createRouterDelta } from './router';
 export { default as siteDelta } from './site';
 
-export const applyDelta = R.pipe(observeDelta, applyMiddleware);
+export const applyDelta = R.pipe(observeDelta, applyMiddleware, composeWithDevTools);
--- client/settings/index.js
+++ client/settings/index.js
@@ -1,4 +1,3 @@
-import R from 'ramda';
 import { createStore, combineReducers } from 'redux';
 import { fromESObservable } from 'kefir';
 import root from './root';
@@ -9,24 +8,13 @@ import { globals, route, prism, gist } from '../reducer';
 const { __GISTPEN_SETTINGS__ } = global;
 
 // eslint-disable-next-line camelcase
-__webpack_public_path__ = __GISTPEN_SETTINGS__.const.url + 'assets/js/';
+__webpack_public_path__ = __GISTPEN_SETTINGS__.globals.url + 'assets/js/';
 
-let enhancer = applyDelta(createRouterDelta(router), siteDelta);
-
-let reducer = combineReducers({
-    globals,
-    route,
-    prism,
-    gist
-});
-
-if (process.env.NODE_ENV !== 'production') {
-    // To use devtools, install Chrome extension:
-    // https://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd
-    enhancer = R.pipe(global.devToolsExtension ? global.devToolsExtension() : R.identity, enhancer);
-}
-
-const store = createStore(reducer, __GISTPEN_SETTINGS__, enhancer);
+const store = createStore(
+    combineReducers({ globals, route, prism, gist }),
+    __GISTPEN_SETTINGS__,
+    applyDelta(createRouterDelta(router), siteDelta)
+);
 const state$ = fromESObservable(store).toProperty(store.getState);
 
 document.addEventListener('DOMContentLoaded', () => {
