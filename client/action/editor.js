35d20e51a0a100df05cf52d2923c2541591d076c
mAAdhaTTah <jamesorodig@gmail.com>
2016-10-22 11:49:36 -0400

    Add additional repo & editor actions
    This are all used by the editor controls to emit actions into the store.

--- client/action/editor.js
+++ client/action/editor.js
@@ -13,3 +13,83 @@ export const EDITOR_OPTIONS_CLICK = 'EDITOR_OPTIONS_CLICK';
 export const editorOptionsClickAction = function editorOptionsClickAction() {
     return { type: EDITOR_OPTIONS_CLICK };
 };
+
+/**
+ * Dispatched when the Editor theme changes.
+ *
+ * @type {string}
+ */
+export const EDITOR_THEME_CHANGE = 'EDITOR_THEME_CHANGE';
+
+/**
+ * Creates a new Editor Options Click Action.
+ *
+ * @param {string} value - Editor theme.
+ * @returns {Action} Editor Options Click Action
+ */
+export const editorThemeChangeAction = function editorThemeChangeAction(value) {
+    return {
+        type: EDITOR_THEME_CHANGE,
+        payload: { value }
+    };
+};
+
+/**
+ * Dispatched when the Editor switches between tabs and spaces.
+ *
+ * @type {string}
+ */
+export const EDITOR_TABS_TOGGLE = 'EDITOR_TABS_TOGGLE';
+
+/**
+ * Creates a new Editor Tabs Change Action.
+ *
+ * @param {string} value - Editor tabs enabled status.
+ * @returns {Action} Editor Options Click Action
+ */
+export const editorTabsToggleAction = function editorTabsToggleAction(value) {
+    return {
+        type: EDITOR_TABS_TOGGLE,
+        payload: { value }
+    };
+};
+
+/**
+ * Dispatched when the Editor indentation width changes.
+ *
+ * @type {string}
+ */
+export const EDITOR_WIDTH_CHANGE = 'EDITOR_WIDTH_CHANGE';
+
+/**
+ * Creates a new Editor Width Change Action.
+ *
+ * @param {string} value - Editor indentation width.
+ * @returns {Action} Editor Options Click Action
+ */
+export const editorWidthChangeAction = function editorWidthChangeAction(value) {
+    return {
+        type: EDITOR_WIDTH_CHANGE,
+        payload: { value }
+    };
+};
+
+/**
+ * Dispatched when the Editor enables or disables invisibles.
+ *
+ * @type {string}
+ */
+export const EDITOR_INVISIBLES_TOGGLE = 'EDITOR_INVISIBLES_TOGGLE';
+
+/**
+ * Creates a new Editor Invisibles Toggle Action.
+ *
+ * @param {string} value - Editor theme.
+ * @returns {Action} Editor Options Click Action
+ */
+export const editorInvisiblesToggleAction = function editorInvisiblesToggleAction(value) {
+    return {
+        type: EDITOR_INVISIBLES_TOGGLE,
+        payload: { value }
+    };
+};
--- client/action/repo.js
+++ client/action/repo.js
@@ -6,14 +6,54 @@
 export const REPO_DESCRIPTION_CHANGE = 'REPO_DESCRIPTION_CHANGE';
 
 /**
- * Creates a new RepdoDescription Change Action.
+ * Creates a new Repo Description Change Action.
  *
  * @param {string} value - Repo description.
- * @returns {Action} RepoDescription Change Action.
+ * @returns {Action} Repo Description Change Action.
  */
 export const repoDescriptionChangeAction = function repoDescriptionChangeAction(value) {
     return {
         type: REPO_DESCRIPTION_CHANGE,
+        payload: { value }
+    };
+};
+
+/**
+ * Dispatched when the Repo status changes.
+ *
+ * @type {string}
+ */
+export const REPO_STATUS_CHANGE = 'REPO_STATUS_CHANGE';
+
+/**
+ * Creates a new Repo Status Change Action.
+ *
+ * @param {string} value - Repo status..
+ * @returns {Action} Repo Status Change Action.
+ */
+export const repoStatusChangeAction = function repoStatusChangeAction(value) {
+    return {
+        type: REPO_STATUS_CHANGE,
+        payload: { value }
+    };
+};
+
+/**
+ * Dispatched when the Repo sync status changes.
+ *
+ * @type {string}
+ */
+export const REPO_SYNC_TOGGLE = 'REPO_SYNC_TOGGLE';
+
+/**
+ * Creates a new Repo Sync Change Action.
+ *
+ * @param {string} value - Repo sync status.
+ * @returns {Action} Repo Sync Change Action.
+ */
+export const repoSyncToggleAction = function repoSyncToggleAction(value) {
+    return {
+        type: REPO_SYNC_TOGGLE,
         payload: { value }
     };
 };
