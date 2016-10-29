ea0ec14db10536b2d60f13fe3396f70dba525d2b
mAAdhaTTah <jamesorodig@gmail.com>
2016-10-29 19:20:29 -0400

    Update reducer to respond to controls
    Uses the new object rest spread operator to create the new state. This
    syntax is lovely...
    
    Update the defaults to include all of the editor state properties.

--- client/reducer/editor.js
+++ client/reducer/editor.js
@@ -1,7 +1,12 @@
-import { EDITOR_OPTIONS_CLICK } from '../action';
+import { EDITOR_OPTIONS_CLICK, EDITOR_INVISIBLES_TOGGLE, EDITOR_THEME_CHANGE,
+    EDITOR_TABS_TOGGLE, EDITOR_WIDTH_CHANGE } from '../action';
 
 const defaults = {
-    optionsOpen: false
+    optionsOpen: false,
+    theme: 'default',
+    tabs: 'off',
+    width: '4',
+    invisibles: 'off'
 };
 
 /**
@@ -9,12 +14,21 @@ const defaults = {
  *
  * @param {Object} state - Current editor state.
  * @param {string} type - Action type.
+ * @param {Object} payload - Action payload.
  * @returns {Object} New editor state.
  */
-export default function editorReducer(state = defaults, { type } = {}) {
+export default function editorReducer(state = defaults, { type, payload } = {}) {
     switch (type) {
         case EDITOR_OPTIONS_CLICK:
-            return Object.assign({}, state, { optionsOpen: !state.optionsOpen });
+            return { ...state, optionsOpen: !state.optionsOpen };
+        case EDITOR_THEME_CHANGE:
+            return { ...state, theme: payload.value };
+        case EDITOR_TABS_TOGGLE:
+            return { ...state, tabs: payload.value };
+        case EDITOR_WIDTH_CHANGE:
+            return { ...state, width: payload.value };
+        case EDITOR_INVISIBLES_TOGGLE:
+            return { ...state, invisibles: payload.value };
         default:
             return state;
     }
