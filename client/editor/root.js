c1daaff9fe19c64c525f21a04105027b5b49c64c
mAAdhaTTah <jamesorodig@gmail.com>
2016-11-12 19:17:47 -0500

    Update root component to render by template
    Use the brookjs/render module.

--- client/editor/root.js
+++ client/editor/root.js
@@ -2,9 +2,11 @@ import './index.scss';
 import R from 'ramda';
 import { component } from 'brookjs';
 import children from 'brookjs/children';
+import render from 'brookjs/render';
 import ControlsComponent from './controls';
 import DescriptionComponent from './description';
 import InstanceComponent from './instance';
+import template from './index.hbs';
 
 export default component({
     children: children({
@@ -15,5 +17,6 @@ export default component({
             key: 'instance.key',
             modifyChildProps: R.map(props => props.editor.instances.map(instance => ({ ...props, instance })))
         }
-    })
+    }),
+    render: render(template)
 });
