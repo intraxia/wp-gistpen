18b09193f4575a030e4e61ecd7e6c9bd4beedf1b
mAAdhaTTah <jamesorodig@gmail.com>
2016-11-12 20:55:29 -0500

    Update repo reducer to pluck information from AJAX
    Ensures the repo key is kept up to date with the server response.

--- client/reducer/index.js
+++ client/reducer/index.js
@@ -3,10 +3,10 @@ import R from 'ramda';
 export { default as editor } from './editor';
 export { default as gist } from './gist';
 export { default as prism } from './prism';
+export { default as repo } from './repo';
 export { default as route } from './route';
 
 const defaultReducer = R.pipe(R.defaultTo({}), R.identity);
 
 export const api = defaultReducer;
 export const globals = defaultReducer;
-export const repo = defaultReducer;
