150948a3ec42942a0745d51f8ac7692f8a88dcf4
mAAdhaTTah <jamesorodig@gmail.com>
2017-01-21 18:26:12 -0500

    Update User to store data in array
    Updates the tests and methods to account for this. Add `User::patch`
    method for use by the UserController. Update test for that as well.

--- app/Options/User.php
+++ app/Options/User.php
@@ -3,13 +3,40 @@ namespace Intraxia\Gistpen\Options;
 
 use InvalidArgumentException;
 
+/**
+ * Class User
+ *
+ * User options service for managing the user options.
+ *
+ * @package    Intraxia\Gistpen
+ * @subpackage Options
+ */
 class User {
 	/**
 	 * Registered options.
 	 *
 	 * @var array
 	 */
-	protected $options = array( 'ace_theme', 'ace_invisibles', 'ace_tabs', 'ace_width' );
+	protected $defaults = array(
+		'editor' => array(
+			'theme'              => 'default',
+			'invisibles_enabled' => 'off',
+			'tabs_enabled'       => 'off',
+			'indent_width'       => '4',
+		),
+	);
+
+	/**
+	 * Legacy user values to map to new values.
+	 *
+	 * @var array
+	 */
+	protected $legacy = array(
+		'ace_theme'      => 'editor.theme',
+		'ace_invisibles' => 'editor.invisibles_enabled',
+		'ace_tabs'       => 'editor.tabs_enabled',
+		'ace_width'      => 'editor.indent_width'
+	);
 
 	/**
 	 * Options prefix.
@@ -24,13 +51,24 @@ class User {
 	 * @return array
 	 */
 	public function all() {
-		$all = array();
+		$options = get_user_meta( get_current_user_id(), 'wpgp_options', true );
 
-		foreach ( $this->options as $option ) {
-			$all[ $option ] = get_user_meta( get_current_user_id(), $this->make_option( $option ), true );
+		if ( ! $options ) {
+			$options = $this->defaults;
 		}
 
-		return $all;
+		foreach ( $this->legacy as $legacy => $path ) {
+			$value = get_user_meta( get_current_user_id(), $this->make_option( $legacy ), true );
+
+			if ( $value ) {
+				$options = $this->set_by_path( $options, $value, $path );
+
+				delete_user_meta( get_current_user_id(), $this->make_option( $legacy ) );
+				update_user_meta( get_current_user_id(), 'wpgp_options', $options );
+			}
+		}
+
+		return $options;
 	}
 
 	/**
@@ -43,27 +81,75 @@ class User {
 	 * @throws InvalidArgumentException
 	 */
 	public function get( $name ) {
-		if ( ! in_array( $name, $this->options, true ) ) {
-			throw new InvalidArgumentException( $name );
+		$value = $this->all();
+		$parts = explode( '.', $name );
+
+		while ( $part = array_shift( $parts ) ) {
+			if ( ! isset( $value[ $part ] ) ) {
+				throw new InvalidArgumentException( $name );
+			}
+
+			$value = $value[ $part ];
 		}
 
-		return get_user_meta( get_current_user_id(), $this->make_option( $name ), true );
+		return $value;
 	}
 
 	/**
 	 * Sets the option value for the current user.
 	 *
-	 * @param string $name
+	 * @param string $path
 	 * @param string $value
+	 *
+	 * @return array Updated options.
 	 *
 	 * @throws InvalidArgumentException
 	 */
-	public function set( $name, $value ) {
-		if ( ! in_array( $name, $this->options, true ) ) {
-			throw new InvalidArgumentException( $name );
+	public function set( $path, $value ) {
+		$options = $this->set_by_path( $this->all(), $value, $path );
+
+		update_user_meta( get_current_user_id(), 'wpgp_options', $options );
+
+		return $options;
+	}
+
+	/**
+	 * Patch the options with the provided array.
+	 *
+	 * @param array $update
+	 *
+	 * @return array
+	 *
+	 * @throws InvalidArgumentException
+	 */
+	public function patch( array $update ) {
+		$options = $this->all();
+
+		foreach ( $update as $key => $value ) {
+			if ( ! isset( $options[ $key ] ) ) {
+				throw new InvalidArgumentException( $key );
+			}
+
+			if ( ! is_array( $value ) ) {
+				throw new InvalidArgumentException( $key );
+			}
+
+			$updating = $options[ $key ];
+
+			foreach ( $value as $next_key => $next_value ) {
+				if ( ! isset( $updating[ $next_key ] ) ) {
+					throw new InvalidArgumentException( $key );
+				}
+
+				$updating[ $next_key ] = $next_value;
+			}
+
+			$options[ $key ] = $updating;
 		}
 
-		update_user_meta( get_current_user_id(), $this->make_option( $name ), $value );
+		update_user_meta( get_current_user_id(), 'wpgp_options', $options );
+
+		return $options;
 	}
 
 	/**
@@ -75,5 +161,33 @@ class User {
 	 */
 	private function make_option( $option ) {
 		return "_{$this->prefix}_{$option}";
+	}
+
+	/**
+	 * Updates the value for the provided array by the given
+	 * dot-deliminted path, e.g. editor.theme will update
+	 * $options['editor']['theme'] = $value.
+	 *
+	 * @param array  $options Options array to update.
+	 * @param mixed  $value   Value to update with.
+	 * @param string $path    Path to update.
+	 *
+	 * @return array Updated options array.
+	 */
+	private function set_by_path( $options, $value, $path ) {
+		$exploded = explode( '.', $path );
+
+		$temp = &$options;
+		foreach ( $exploded as $key ) {
+			if ( ! isset( $temp[ $key ] ) ) {
+				throw new InvalidArgumentException( $path );
+			}
+
+			$temp = &$temp[ $key ];
+		}
+		$temp = $value;
+		unset( $temp );
+
+		return $options;
 	}
 }
--- app/View/Editor.php
+++ app/View/Editor.php
@@ -153,10 +153,10 @@ class Editor implements HasActions, HasFilters {
 						),
 					);
 				}, $blobs ),
-				'width'      => $this->user->get( 'ace_width' ),
-				'theme'      => $this->user->get( 'ace_theme' ),
-				'invisibles' => $this->user->get( 'ace_invisibles' ) ? : 'off',
-				'tabs'       => $this->user->get( 'ace_tabs' ) ? : 'off',
+				'width'      => $this->user->get( 'editor.indent_width' ),
+				'theme'      => $this->user->get( 'editor.theme' ),
+				'invisibles' => $this->user->get( 'editor.invisibles_enabled' ) ? : 'off',
+				'tabs'       => $this->user->get( 'editor.tabs_enabled' ) ? : 'off',
 				'widths'     => array( '1', '2', '4', '8' ),
 				'themes'     => array(
 					'default'                         => __( 'Default', 'wp-gistpen' ),
