15d9235086ca8e235bf19dc59feb830164412f96
mAAdhaTTah <jamesorodig@gmail.com>
2017-02-25 22:00:47 -0500

    Add Flow & typechecks to dom selectors
    These are responsible for fetching offset information for an element.

--- client/selector/dom.js
+++ client/selector/dom.js
@@ -1,19 +1,21 @@
+// @flow
+
 /**
  * Get the selection start location for the given element.
  *
  * @param {Element} node - Element to check for selection start.
  * @returns {number} Selection start.
  */
-export function selectSelectionStart(node) {
+export function selectSelectionStart(node : Element) : number {
     const selection = getSelection();
 
-    if (selection.rangeCount) {
+    if (selection && selection.rangeCount) {
         let range = selection.getRangeAt(0);
         let element = range.startContainer;
         let container = element;
         let offset = range.startOffset;
 
-        if (!(node.compareDocumentPosition(element) & 0x10)) {
+        if (!container || !(node.compareDocumentPosition(element) & 0x10)) {
             return 0;
         }
 
@@ -25,7 +27,7 @@ export function selectSelectionStart(node) {
             }
 
             element = container = container.parentNode;
-        } while (element && element !== node);
+        } while (container && element && element !== node);
 
         return offset;
     } else {
@@ -39,11 +41,11 @@ export function selectSelectionStart(node) {
  * @param {Element} node - Element to check for selection start.
  * @returns {number} Selection end.
  */
-export function selectSelectionEnd(node) {
+export function selectSelectionEnd(node : Element) : number {
     const selection = getSelection();
 
-    if (selection.rangeCount) {
-        return selectSelectionStart(node) + (selection.getRangeAt(0) + '').length;
+    if (selection && selection.rangeCount) {
+        return selectSelectionStart(node) + selection.getRangeAt(0).toString().length;
     } else {
         return 0;
     }
