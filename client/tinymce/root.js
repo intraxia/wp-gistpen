a9c85757240c25a78bd484235aadc5320b13f3a6
mAAdhaTTah <jamesorodig@gmail.com>
2017-02-26 23:22:34 -0500

    Add root component to mount any added children
    The root component will mount the body and bind any added children
    to the root stream. We'll side effect into the DOM as a result of the
    popup opening and the root stream can mount and control it
    automatically.

---
+++ client/tinymce/root.js
@@ -0,0 +1,9 @@
+// @flow
+import { component, children } from 'brookjs';
+import { SearchComponent } from '../component';
+
+export default component({
+    children: children({
+        search: SearchComponent
+    })
+});
