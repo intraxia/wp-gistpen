b36feeb6a0953f98597f6efe898f86834cf1c2f7
mAAdhaTTah <jamesorodig@gmail.com>
2017-03-05 14:06:05 -0500

    Remove unnecessary dependencies and dead code
    Everything in `src/` has been rewritten in the `client` directory with
    brookjs, so remove it and any dependencies left behind.

--- src/js/tinymce.js
+++
@@ -1,5 +0,0 @@
-/**
- * Register plugin with TinyMCE.
- */
-tinymce.create('tinymce.plugins.wp_gistpen', {init: require('./button')});
-tinymce.PluginManager.add('wp_gistpen', tinymce.plugins.wp_gistpen);
