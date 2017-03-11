c0d8b4899eac5d324d25d8115a6dc2afd6cfb72c
mAAdhaTTah <jamesorodig@gmail.com>
2017-03-10 22:43:07 -0500

    Save search results into the store
    Update the reducer to save the search results from the API.

--- client/reducer/search.js
+++ client/reducer/search.js
@@ -1,11 +1,15 @@
 // @flow
-import type { SearchInputAction, SearchState } from '../type';
+import type { SearchInputAction, SearchResultsSucceededAction, SearchState } from '../type';
 import { combineActionReducers } from 'brookjs';
-import { SEARCH_INPUT } from '../action';
+import { SEARCH_INPUT, SEARCH_RESULTS_SUCCEEDED } from '../action';
 
 export default combineActionReducers([
     [SEARCH_INPUT, (state : SearchState, action : SearchInputAction) => ({
         ...state,
         term: action.payload.value
+    })],
+    [SEARCH_RESULTS_SUCCEEDED, (state : SearchState, action : SearchResultsSucceededAction) => ({
+        ...state,
+        results: action.payload.response
     })]
-], { term: '' });
+], { term: '', results: false });
