67c15c20bfc7b399781e77f76371aae1c7bc1108
mAAdhaTTah <jamesorodig@gmail.com>
2017-04-01 13:30:06 -0400

    Add flow to gist reducer
    Use combineActionReducers to simplify the logic.

--- client/reducer/gist.js
+++ client/reducer/gist.js
@@ -1,20 +1,13 @@
+// @flow
+import type { GistState, GistTokenChangeAction } from '../type';
+import { combineActionReducers } from 'brookjs';
 import { GIST_TOKEN_CHANGE } from '../action';
 
 const defaults = { token: '' };
 
-/**
- * Updates the gist state.
- *
- * @param {Object} state - Gist state.
- * @param {string} type - Action type.
- * @param {Object} payload - Action payload.
- * @returns {Object} New gist state.
- */
-export default function gistReducer(state = defaults, { type, payload }) {
-    switch (type) {
-        case GIST_TOKEN_CHANGE:
-            return Object.assign({}, state, { token: payload.value });
-        default:
-            return state;
-    }
-}
+export default combineActionReducers([
+    [GIST_TOKEN_CHANGE, (state : GistState, { payload } : GistTokenChangeAction) => ({
+        ...state,
+        token: payload.value
+    })]
+], defaults);
