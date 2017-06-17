850ddba669248906f592d01b8c707789e63f9d92
mAAdhaTTah <jamesorodig@gmail.com>
2017-06-16 20:03:50 -0400

    Handle routing in the pages
    The page template can now use the `route` parameter to render the
    correct component in the template. We're going to update the state
    with the value from the query parameter and use push state to
    update the page.

--- client/page/edit/index.js
+++ client/page/edit/index.js
@@ -3,8 +3,8 @@ import '../../polyfills';
 import { createStore, combineReducers } from 'redux';
 import { domDelta } from 'brookjs';
 import { applyDelta, repoDelta, revisionsDelta, userDelta } from '../../delta';
-import { api, editor, repo } from '../../reducer';
 import { el, view } from './dom';
+import { api, editor, repo, route } from '../../reducer';
 import { selectEditorProps as selectProps } from '../../selector';
 import { ajax$ } from '../../service';
 
@@ -14,7 +14,7 @@ const { __GISTPEN_EDITOR__ } = global;
 __webpack_public_path__ = __GISTPEN_EDITOR__.api.url + 'assets/js/';
 
 createStore(
-    combineReducers({ api, editor, repo }),
+    combineReducers({ api, editor, repo, route }),
     __GISTPEN_EDITOR__,
     applyDelta(
         domDelta({ el, selectProps, view }),
