531b98bb6389f527bfdbeb5936dc6a383b3f7e8f
mAAdhaTTah <jamesorodig@gmail.com>
2017-07-22 18:53:40 -0400

    Add languages.json and use in JS
    We'll eventually migrate this over to being used by the BE
    as well.

--- client/helpers/prism_slug.js
+++ client/helpers/prism_slug.js
@@ -1,18 +1,4 @@
-/**
- * Make of language slugs to prism slugs.
- *
- * @todo extract to JSON file for reuse.
- *
- * @type {Object}
- */
-const map = {
-    'js': 'javascript',
-    'sass': 'scss',
-    'py': 'python',
-    'html': 'markup',
-    'xml': 'markup',
-    'plaintext': 'none'
-};
+import langs from '../languages.json';
 
 /**
  * Map a language slug to its prism slug.
@@ -21,6 +7,6 @@ const map = {
  * @returns {string} Prism slug.
  */
 export default function prismSlug(slug) {
-    return map[slug] || slug;
+    return langs.aliases[slug] || slug;
 
 };
