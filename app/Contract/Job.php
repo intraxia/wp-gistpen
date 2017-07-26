8e7d2d14a0285e20b693571f914d02e40b0fc113
mAAdhaTTah <jamesorodig@gmail.com>
2017-07-26 09:00:46 -0400

    Add Job Contract
    This assumes the Job is responsible for managing its entire
    lifecylce, kicking itself off, handling the processing, and
    continuing itself if it doesn't finish in a single process.
    This is probably not ideal long-term, as we should move
    some of this handling out of an individual Job and into the
    supporting cast, but we can put that functionality in an
    AbstractJob we extend from for now and break those
    pieces out into separate classes later. It doesn't mean the
    Job cannot be reused outside of Gistpen until that's settled
    but right now we just want to get the functionality working.

---
+++ app/Contract/Job.php
@@ -0,0 +1,54 @@
+<?php
+namespace Intraxia\Gistpen\Contract;
+
+use Intraxia\Gistpen\Jobs\Status;
+use Intraxia\Gistpen\Model\Run;
+use Intraxia\Jaxion\Axolotl\Collection;
+use Intraxia\Jaxion\Contract\Axolotl\Serializes;
+use WP_Error;
+
+interface Job extends Serializes {
+
+	/**
+	 * Get the current status of the job.
+	 *
+	 * @return Status
+	 */
+	public function get_status();
+
+	/**
+	 * Dispatch a job to process the provided data.
+	 *
+	 * Will process all possible data if nothing is provided.
+	 *
+	 * @param Collection|null $items
+	 *
+	 * @return Run|WP_Error
+	 */
+	public function dispatch( Collection $items = null );
+
+	/**
+	 * Fetches a Run by its run id.
+	 *
+	 * @param $run_id
+	 *
+	 * @return Run|WP_Error
+	 */
+	public function fetch( $run_id );
+
+	/**
+	 * Process a batch of data.
+	 *
+	 * @param int $run_id
+	 *
+	 * @return mixed
+	 */
+	public function process();
+
+	/**
+	 * Get the Collection of runs for the job.
+	 *
+	 * @return Collection
+	 */
+	public function runs();
+}
