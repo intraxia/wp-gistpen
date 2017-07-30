4bb84c8d00706f360dc9f2677bd143b33daaa74b
mAAdhaTTah <jamesorodig@gmail.com>
2017-07-30 10:36:16 -0400

    Create Jobs screens with tables of runs/messages
    Using the WP built-in styles for this for now, although we
    could easily move to using something else for the
    messages–something that looks more "console-y".

--- client/page/settings/view.js
+++ client/page/settings/view.js
@@ -5,7 +5,6 @@ import { component, children, render } from 'brookjs';
 import header from './header';
 import highlighting from './highlighting';
 import accounts from './accounts';
-import jobs from './jobs';
 import template from './index.hbs';
 
 const prismChanged = (prev : SettingsProps, next : SettingsProps) : boolean =>
@@ -21,7 +20,6 @@ export default component({
             factory: highlighting,
             modifyChildProps: (props$ : Observable<SettingsProps>) : Observable<HasPrismState> => props$.skipDuplicates(prismChanged)
         },
-        settingsAccounts: accounts,
-        settingsJobs: jobs
+        settingsAccounts: accounts
     })
 });
