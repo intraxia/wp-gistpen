b6cc7792c9c716c027ee100292026a9b66166e37
mAAdhaTTah <jamesorodig@gmail.com>
2017-08-02 00:03:27 -0400

    Create Gist client service
    This is responsible for sending HTTP requests. I wrote a
    small script that I ran with wp eval-file test-client.php,
    which gave me access to my site's internals so I could
    just use the token and implementation there. I don't plan
    on setting this up via CI, but it is useful to check that it
    does what we think it does. Unit testing the client would
    be the next real step.

---
+++ app/Providers/ClientServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+namespace Intraxia\Gistpen\Providers;
+
+use Intraxia\Gistpen\Client\Gist;
+use Intraxia\Jaxion\Contract\Core\Container;
+use Intraxia\Jaxion\Contract\Core\ServiceProvider;
+use Requests_Session;
+
+class ClientServiceProvider implements ServiceProvider {
+
+	/**
+	 * Register the provider's services on the container.
+	 *
+	 * This method is passed the container to register on, giving the service provider
+	 * an opportunity to register its services on the container in an encapsulated way.
+	 *
+	 * @param Container $container
+	 */
+	public function register( Container $container ) {
+		$container->define( 'client.gist', function( Container $container ) {
+			return new Gist( $container->fetch( 'options.site' ), new Requests_Session );
+		} );
+	}
+}
