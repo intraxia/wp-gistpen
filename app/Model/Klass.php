cee7b070739b55d589c777fb208e02361dad0d27
mAAdhaTTah <jamesorodig@gmail.com>
2017-08-05 13:17:40 -0400

    Move Save class logic to Listener\Database
    Eliminate some more stuff we don't need here.

--- app/Model/Klass.php
+++ app/Model/Klass.php
@@ -5,6 +5,7 @@ use MyCLabs\Enum\Enum;
 
 class Klass extends Enum {
 	const REPO = 'Intraxia\Gistpen\Model\Repo';
+	const BLOB = 'Intraxia\Gistpen\Model\Blob';
 	const RUN = 'Intraxia\Gistpen\Model\Run';
 	const MESSAGE = 'Intraxia\Gistpen\Model\Message';
 	const STATE = 'Intraxia\Gistpen\Model\State';
--- app/Providers/CoreServiceProvider.php
+++ app/Providers/CoreServiceProvider.php
@@ -1,11 +1,7 @@
 <?php
 namespace Intraxia\Gistpen\Providers;
 
-use Intraxia\Gistpen\Facade\Adapter;
-use Intraxia\Gistpen\Facade\Database;
-use Intraxia\Gistpen\Migration;
 use Intraxia\Gistpen\Register\Data;
-use Intraxia\Gistpen\Save;
 use Intraxia\Jaxion\Contract\Core\Container;
 use Intraxia\Jaxion\Contract\Core\ServiceProvider;
 
@@ -22,16 +18,8 @@ class CoreServiceProvider implements ServiceProvider {
 	 * @param Container $container
 	 */
 	public function register( Container $container ) {
-		$container
-			->define( 'register.data', new Data )
-			->define( 'facade.adapter', new Adapter )
-			->define( 'facade.database', new Database( $container->fetch( 'facade.adapter' ) ) )
-			->define(
-				'save',
-				new Save(
-					$container->fetch( 'facade.database' ),
-					$container->fetch( 'facade.adapter' )
-				)
-			);
+		$container->define( 'register.data', function () {
+			return new Data;
+		} );
 	}
 }
