8d13955c7a26442fa5e1173f81eff8f0be6e7f31
mAAdhaTTah <jamesorodig@gmail.com>
2018-01-01 10:34:40 -0500

    Move build process into beaver
    Includes copying over the files from prism using copy-webpack-plugin.
    Also update the scripts in `package.json`. We won't use these yet
    until we've moved everything into webpack, including the .pot
    generation.

--- .beaverrc.js
+++ .beaverrc.js
@@ -1,3 +1,12 @@
+import path from 'path';
+import FlowStatusWebpackPlugin from 'flow-status-webpack-plugin';
+import StyleLintPlugin from 'stylelint-webpack-plugin';
+import WebpackNotifierPlugin from 'webpack-notifier';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import flowPath from 'flow-bin';
+import gutil from 'gulp-util';
+import notifier from 'node-notifier';
+
 export const dir = 'client';
 
 /**
@@ -13,16 +22,83 @@ export const mocha = {
     ]
 };
 
+const client = path.resolve(__dirname, dir);
+const pages = path.resolve(client, 'pages');
+
 /**
  * Webpack build configuration.
  */
 export const webpack = {
     entry: {
-        app: 'app.js'
+        settings: 'pages/settings',
+        content: 'pages/content',
+        editor: 'pages/edit',
+        tinymce: 'pages/tinymce'
     },
     output: {
-        path: 'dist/',
+        path: 'assets/js/',
         filename: '[name].js'
     },
-    modifier: x => x
+    modifier: (config, state) => {
+        config.module.rules.push({
+            test: /\.(scss|css)$/,
+            include: [
+                path.resolve(pages, 'editor'),
+                path.resolve(pages, 'tinymce'),
+                path.resolve(client, 'component'),
+            ],
+            use: [{
+                loader: 'style-loader',
+                options: {
+                    hmr: false
+                }
+            }, 'css-loader', 'sass-loader']
+        });
+        config.module.rules.push({
+            test: /\.(scss|css)$/,
+            include: [
+                path.resolve(pages, 'settings'),
+                /prism/
+            ],
+            use: [{
+                loader: 'style-loader/useable',
+                options: {
+                    hmr: false
+                }
+            }, 'css-loader', 'sass-loader']
+        });
+
+        config.plugins.push(new FlowStatusWebpackPlugin({
+            binaryPath: flowPath,
+            onSuccess: stdout => {
+                gutil.log('[webpack:flow]', stdout);
+
+                notifier.notify({title: 'Flow', message: 'Flow passed'});
+            },
+            onError: stdout => {
+                gutil.log('[webpack:flow]', stdout);
+
+                notifier.notify({title: 'Flow', message: 'Flow failed'});
+            }
+        }));
+        config.plugins.push(new StyleLintPlugin({ syntax: 'scss' }));
+        config.plugins.push(new WebpackNotifierPlugin({alwaysNotify: true}));
+        config.plugins.push(new CopyWebpackPlugin([{
+            from: 'node_modules/prismjs/components/*.js',
+            flatten: true,
+        }]));
+
+        return config;
+    }
+};
+
+export const storybook = {
+    port: 9001,
+    host: null,
+    staticDirs: ['assets/js'],
+    https: {
+        enabled: false
+    },
+    devServer: {},
+    middleware: (router, state) => router
 };
