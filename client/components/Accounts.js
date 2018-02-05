bb9df191c738ee890bca1a3744ad045277ff1e5f
mAAdhaTTah <jamesorodig@gmail.com>
2018-02-04 19:59:50 -0500

    Add Accounts component
    Based off the Handlebars component.

---
+++ client/components/Accounts.js
@@ -0,0 +1,36 @@
+// @flow
+// @jsx h
+import { Collector, h, view } from 'brookjs-silt';
+import { gistTokenChange } from '../actions';
+
+type AccountProps = {
+    token: string
+};
+
+export default ({ stream$ }: ObservableProps<AccountProps>) => (
+    <Collector>
+        <div className="table" data-brk-container="settingsAccounts">
+            <h3 className="title">Sync Account Settings</h3>
+            <table className="form-table">
+                <tbody>
+                    <tr>
+                        <th>
+                            <label htmlFor="wpgp-token">Gist Token</label>
+                        </th>
+                        <td>
+                            <input type="text" name="wpgp-token"
+                                id="wpgp-token" className="regular-text"
+                                onChange={evt$ => evt$.map((e: SyntheticInputEvent<*>) => gistTokenChange(e.target.value))}
+                                value={stream$.thru(view((prop: AccountProps) => prop.token))}/>
+                            <p className="description" id="wpgp-token-description">
+                            Create a new <a href="https://github.com/settings/tokens" target="_blank">Personal Access
+                            Token</a>
+                            with <code>gist</code> scope and paste it here.
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </Collector>
+);
