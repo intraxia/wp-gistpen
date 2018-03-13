092aad7b207db7d518def4e38bbea8e48acf1886
mAAdhaTTah <jamesorodig@gmail.com>
2018-03-13 17:53:22 -0400

    1.0.0-beta.9

--- app/App.php
+++ app/App.php
@@ -22,7 +22,7 @@ class App extends Application {
 	/**
 	 * Plugin version constant.
 	 */
-	const VERSION = '1.0.0-beta.8';
+	const VERSION = '1.0.0-beta.9';
 
 	/**
 	 * ServiceProviders to register with the Application
