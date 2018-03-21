ae0389167b382f661101fc76fa80b006aa51b97e
mAAdhaTTah <jamesorodig@gmail.com>
2018-03-21 18:27:31 -0400

    Adjust repo component props
    Use the new Loopable construct.

--- client/components/Repo.js
+++ client/components/Repo.js
@@ -1,15 +1,14 @@
 // @flow
 import type { Observable } from 'kefir';
-import type { Toggle } from '../types';
 import { h } from 'brookjs-silt';
 import { Fragment } from 'react';
 import Blob from './Blob';
 
 type RepoProps = {
     blobs: {
-        order: Array<number>;
+        order: Array<string>;
         dict: {
-            [key: number]: {
+            [key: string]: {
                 code: string;
                 filename: string;
                 language: string
@@ -18,8 +17,8 @@ type RepoProps = {
     };
     prism: {
         theme: string;
-        'line-numbers': number;
-        'show-invisibles': Toggle
+        'line-numbers': boolean;
+        'show-invisibles': boolean
     }
 };
 
@@ -27,7 +26,7 @@ export default ({ stream$ }: { stream$: Observable<RepoProps> }) => (
     <Fragment>
         {/* $FlowFixMe */}
         <div>
-            {stream$.map(({ blobs }) => blobs.order.map(id => (
+            {stream$.map((props: RepoProps) => props.blobs.order.map(id => (
                 <Fragment>
                     {/* $FlowFixMe */}
                     <Blob key={id} stream$={stream$.map(({ blobs, prism }) => ({
