fad77da0f4f382b749f5d600fdf0b16b8f8aa4d0
mAAdhaTTah <jamesorodig@gmail.com>
2018-03-21 22:15:29 -0400

    Update the ajax reducer to set loading
    These generic actions should set whether the AJAX system is
    currently running.

--- client/reducers/ajax.js
+++ client/reducers/ajax.js
@@ -1,18 +1,18 @@
 // @flow
 import type { Reducer } from 'redux';
-import type { SearchInputAction } from '../types';
+import type { AjaxState, SearchInputAction } from '../types';
 import { combineActionReducers } from 'brookjs';
-import { SEARCH_INPUT, SEARCH_RESULTS_SUCCEEDED } from '../actions';
-
-export type AjaxState = {
-    running: boolean
-};
+import { AJAX_STARTED, AJAX_FAILED, AJAX_FINISHED,
+    SEARCH_INPUT, SEARCH_RESULTS_SUCCEEDED } from '../actions';
 
 const defaults : AjaxState = {
     running: false
 };
 
 const cond = [
+    [AJAX_STARTED, (state: AjaxState) => ({ ...state, running: true })],
+    [AJAX_FAILED, (state: AjaxState) => ({ ...state, running: false })],
+    [AJAX_FINISHED, (state: AjaxState) => ({ ...state, running: false })],
     [SEARCH_INPUT, (state: AjaxState, { payload }: SearchInputAction) => ({ ...state, running: !!payload.value })],
     [SEARCH_RESULTS_SUCCEEDED, (state: AjaxState) => ({ ...state, running: false })]
 ];
