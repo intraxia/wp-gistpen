7b18b1659071b55c412177524277259cd161e195
mAAdhaTTah <jamesorodig@gmail.com>
2018-06-03 18:07:53 -0400

    Flatten Promise chain in old-style blob
    It's no longer necessary to sequence, since order no longer matters.

--- client/components/blob/index.js
+++ client/components/blob/index.js
@@ -8,11 +8,12 @@ export default component({
     onMount: (el, props$): Observable<void> => {
         Prism.setAutoloaderPath(__webpack_public_path__);
 
-        return props$.flatMap((props: HasPrismState): Observable<void> => {
-            const promise = Prism.setTheme(props.prism.theme).then(() => Promise.all([
+        return props$.flatMapLatest((props: HasPrismState): Observable<void> => {
+            const promise = Promise.all([
+                Prism.setTheme(props.prism.theme),
                 Prism.togglePlugin('line-numbers', props.prism['line-numbers']),
                 Prism.togglePlugin('show-invisibles', props.prism['show-invisibles'])
-            ]));
+            ]);
 
             return Kefir.fromPromise(promise).flatMap(() => raf$.take(1).flatMap((): Observable<void> => Kefir.stream((emitter: Emitter<void, void>) => {
                 Prism.highlightElement(el.querySelector('code'), false);
