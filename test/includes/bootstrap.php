f3bf8d4fc0686c63f0774f5463f76f1c7a35f0e5
mAAdhaTTah <jamesorodig@gmail.com>
2018-12-29 09:38:45 -0500

    Run front-end tests in separate build
    Extract before_build steps to script so only front end tasks get run with
    the front end build.

--- test/includes/bootstrap.php
+++ test/includes/bootstrap.php
@@ -22,15 +22,48 @@ if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 // Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 
+$plugin_root = dirname( dirname( dirname( __FILE__ ) ) );
+
 /**
  * Manually load the plugin being tested.
  */
-function _manually_load_plugin() {
-	require dirname( dirname( dirname( __FILE__ ) ) ) . '/wp-gistpen.php';
-}
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+$_manually_load_plugin = function() use ( $plugin_root ) {
+	require $plugin_root . '/wp-gistpen.php';
+};
+
+tests_add_filter( 'muplugins_loaded', $_manually_load_plugin );
 
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
 
+$languages_config = $plugin_root . '/config/languages.json';
+
+$exists = file_exists( $languages_config );
+
+if ( ! $exists ) {
+	$dummy = array(
+		'list' =>  array(
+			'js'  => 'JavaScript',
+			'php' => 'PHP',
+		),
+		'aliases' => array(
+			'js'  => 'javascript',
+		),
+	);
+
+	file_force_contents( $languages_config, json_encode( $dummy ) );
+}
+
 App::instance()->activate();
+
+function file_force_contents( $dir, $contents ) {
+	$parts = explode( '/', $dir );
+	$file = array_pop( $parts );
+	$dir = '';
+	foreach( $parts as $part ) {
+		if( ! is_dir( $dir .= "/$part" ) ) {
+			mkdir($dir);
+		}
+	}
+	file_put_contents("$dir/$file", $contents);
+}
