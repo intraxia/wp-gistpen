734d297f6c52c5cab3ab7536751cf80a5090927c
mAAdhaTTah <jamesorodig@gmail.com>
2019-05-25 20:22:32 -0400

    Convert Search component to TypeScript

---
+++ client/components/SearchPopup/SearchPopup.story.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import SearchPopup from './';
+
+storiesOf('SearchPopup', module)
+  .add('without results; not loading', () => (
+    <SearchPopup
+      {...{
+        loading: false,
+        term: '',
+        results: []
+      }}
+    />
+  ))
+  .add('with results; not loading', () => (
+    <SearchPopup
+      {...{
+        loading: false,
+        term: 'test',
+        results: [
+          {
+            id: '1',
+            filename: 'test1.js'
+          },
+          {
+            id: '2',
+            filename: 'test2.js'
+          }
+        ]
+      }}
+    />
+  ))
+  .add('without results; loading', () => (
+    <SearchPopup
+      {...{
+        loading: true,
+        term: '',
+        results: []
+      }}
+    />
+  ))
+  .add('with results; loading', () => (
+    <SearchPopup
+      {...{
+        loading: true,
+        term: 'test',
+        results: [
+          {
+            id: '1',
+            filename: 'test1.js'
+          },
+          {
+            id: '2',
+            filename: 'test2.js'
+          }
+        ]
+      }}
+    />
+  ));
