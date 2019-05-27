26fb40a7fd11dd493b3e0b0e2c26ca223192e003
mAAdhaTTah <jamesorodig@gmail.com>
2019-05-27 16:07:19 -0400

    Convert Commits to TypeScript

---
+++ client/components/Commits/Commits.story.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Commits from './';
+
+storiesOf('Commits', module).add('default', () => (
+  <Commits
+    {...{
+      prism: {
+        theme: 'default',
+        'line-numbers': false,
+        'show-invisibles': false
+      },
+      selectedCommit: {
+        description: 'Commit Description',
+        states: [
+          {
+            ID: '1',
+            code: 'console.log("test");',
+            filename: 'test.js',
+            language: 'javascript'
+          },
+          {
+            ID: '2',
+            code: 'console.log("test");',
+            filename: 'test.js',
+            language: 'javascript'
+          }
+        ]
+      },
+      commits: [
+        {
+          ID: '1',
+          selected: true,
+          description: 'Commit Description',
+          committedAt: '1970-01-01',
+          author: {
+            name: 'Commit Author',
+            avatar: 'http://via.placeholder.com/48x48'
+          }
+        },
+        {
+          ID: '2',
+          selected: false,
+          description: 'Commit Description',
+          committedAt: '1970-01-01',
+          author: {
+            name: 'Commit Author',
+            avatar: 'http://via.placeholder.com/48x48'
+          }
+        }
+      ]
+    }}
+  />
+));
