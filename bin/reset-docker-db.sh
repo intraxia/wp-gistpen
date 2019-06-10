e36795653dba0b233eeeeec9b2716f4160c49c2e
mAAdhaTTah <jamesorodig@gmail.com>
2019-06-10 07:29:14 -0400

    Add classic editor as default plugin
    This is necessary to test the TinyMCE plugin on later versions of WP.

--- bin/reset-docker-db.sh
+++ bin/reset-docker-db.sh
@@ -10,4 +10,5 @@ docker-compose exec app wp core install --allow-root \
 docker-compose exec app wp plugin activate wp-gistpen --allow-root
 docker-compose exec app wp plugin delete akismet --allow-root
 docker-compose exec app wp plugin delete hello.php --allow-root
+docker-compose exec app wp plugin install classic-editor --allow-root
 docker-compose exec app wp rewrite structure '/%postname%/' --allow-root
