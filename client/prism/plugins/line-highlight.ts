ec26ebca8cac5873dc3402f2ca7b257bd5dcba41
mAAdhaTTah <jamesorodig@gmail.com>
2019-11-10 11:30:23 -0500

    Format files for new prettier

--- client/prism/plugins/line-highlight.ts
+++ client/prism/plugins/line-highlight.ts
@@ -1,4 +1,2 @@
 import 'prismjs/plugins/line-highlight/prism-line-highlight';
-export {
-  default as plugin
-} from 'prismjs/plugins/line-highlight/prism-line-highlight.css';
+export { default as plugin } from 'prismjs/plugins/line-highlight/prism-line-highlight.css';
--- client/prism/plugins/show-invisibles.ts
+++ client/prism/plugins/show-invisibles.ts
@@ -1,4 +1,2 @@
 import 'prismjs/plugins/show-invisibles/prism-show-invisibles';
-export {
-  default as plugin
-} from 'prismjs/plugins/show-invisibles/prism-show-invisibles.css';
+export { default as plugin } from 'prismjs/plugins/show-invisibles/prism-show-invisibles.css';
