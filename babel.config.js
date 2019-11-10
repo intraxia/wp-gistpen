6fe74d33e6dee147691549292dafd922c007e3d0
mAAdhaTTah <jamesorodig@gmail.com>
2019-11-10 15:02:59 -0500

    Fix editorconfig for JS files
    These should be indented 2 spaces, not 4.

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,24 @@
 module.exports = api => {
-    api.cache(true);
+  api.cache(true);
 
-    return {
-        presets: [
-            'brookjs',
-            '@babel/typescript',
-            [
-                '@babel/env',
-                {
-                    modules: false,
-                    targets: {
-                        esmodules: true
-                    }
-                }
-            ]
-        ],
-        plugins: [
-            '@babel/plugin-proposal-class-properties',
-            '@babel/syntax-dynamic-import',
-            process.env.NODE_ENV === 'test' &&
-                '@babel/transform-modules-commonjs'
-        ].filter(Boolean)
-    };
+  return {
+    presets: [
+      'brookjs',
+      '@babel/typescript',
+      [
+        '@babel/env',
+        {
+          modules: false,
+          targets: {
+            esmodules: true
+          }
+        }
+      ]
+    ],
+    plugins: [
+      '@babel/plugin-proposal-class-properties',
+      '@babel/syntax-dynamic-import',
+      process.env.NODE_ENV === 'test' && '@babel/transform-modules-commonjs'
+    ].filter(Boolean)
+  };
 };
