c27a2a27681561f919c114b400dc7917feec53c6
mAAdhaTTah <jamesorodig@gmail.com>
2019-12-01 19:16:04 -0500

    Add SKIP_IMAGE_TESTS to reduce image test runs
    This should speed up CI while still building some comfor that
    things are working correectly.

--- e2e/content.spec.ts
+++ e2e/content.spec.ts
@@ -34,8 +34,12 @@ describe('content', () => {
       throw new Error(`Failed to parse repo url from output: ${run.stdout}`);
     }
 
+    const jsDockerPath = `/var/www/src/wp-content/plugins/wp-gistpen/resources/samples/js`;
+
     run = await execa.command(
-      `npm run env cli gistpen blob create -- --repo_id=${repoId} --filename='placeholder' --porcelain`
+      process.env.SKIP_IMAGE_TESTS !== 'true'
+        ? `npm run env cli gistpen blob create -- --repo_id=${repoId} --filename='placeholder' --porcelain`
+        : `npm run env cli gistpen blob create ${jsDockerPath}  -- --repo_id=${repoId} --filename='js' --language='js' --porcelain`
     );
 
     blobId =
@@ -50,6 +54,10 @@ describe('content', () => {
   });
 
   for (let [slug, displayName] of Object.entries(languages.list)) {
+    if (process.env.SKIP_IMAGE_TESTS === 'true' && slug !== 'js') {
+      continue;
+    }
+
     const localPath = require('path').join(
       __dirname,
       '..',
@@ -69,9 +77,11 @@ describe('content', () => {
         throw new Error('Setup failed');
       }
 
-      await execa.command(
-        `npm run env cli gistpen blob update ${blobId} ${dockerPath} -- --filename='${slug}' --language='${slug}'`
-      );
+      if (process.env.SKIP_IMAGE_TESTS !== 'true') {
+        await execa.command(
+          `npm run env cli gistpen blob update ${blobId} ${dockerPath} -- --filename='${slug}' --language='${slug}'`
+        );
+      }
 
       await page.goto(repoUrl);
 
