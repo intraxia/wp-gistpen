fb5534e328d69e6596f16a6db3dc318290167fc2
mAAdhaTTah <jamesorodig@gmail.com>
2019-12-07 23:46:09 -0500

    Lift user id fetching out of UserOptions
    Make this more flexible to places where there isn't
    a current user.

--- test/Unit/Options/UserTest.php
+++ test/Unit/Options/UserTest.php
@@ -31,61 +31,60 @@ class UserTest extends TestCase {
 		parent::setUp();
 		$this->user = new User();
 
-		$user_id = $this->factory->user->create( array( 'role' => 'administrator' ) );
-		wp_set_current_user( $user_id );
-		update_user_meta( $user_id, 'wpgp_options', $this->dummy );
+		$this->user_id = $this->factory->user->create( array( 'role' => 'administrator' ) );
+		update_user_meta( $this->user_id, 'wpgp_options', $this->dummy );
 	}
 
 	public function test_should_retrieve_all_users_options() {
-		$this->assertSame( $this->dummy, $this->user->all() );
+		$this->assertSame( $this->dummy, $this->user->all( $this->user_id ) );
 	}
 
 	public function test_should_retrieve_editor_theme() {
-		$this->assertSame( $this->dummy['editor']['theme'], $this->user->get( 'editor.theme' ) );
+		$this->assertSame( $this->dummy['editor']['theme'], $this->user->get( $this->user_id, 'editor.theme' ) );
 	}
 
 	public function test_should_update_editor_theme() {
 		$value = 'newtest';
 
-		$this->user->set( 'editor.theme', $value );
+		$this->user->set( $this->user_id, 'editor.theme', $value );
 
-		$this->assertSame( $value, $this->user->get( 'editor.theme' ) );
+		$this->assertSame( $value, $this->user->get( $this->user_id, 'editor.theme' ) );
 	}
 
 	public function test_should_retrieve_editor_invisibles_enabled() {
-		$this->assertSame( $this->dummy['editor']['invisibles_enabled'], $this->user->get( 'editor.invisibles_enabled' ) );
+		$this->assertSame( $this->dummy['editor']['invisibles_enabled'], $this->user->get( $this->user_id, 'editor.invisibles_enabled' ) );
 	}
 
 	public function test_should_update_editor_invisibles_enabled() {
 		$value = 'off';
 
-		$this->user->set( 'editor.invisibles_enabled', $value );
+		$this->user->set( $this->user_id, 'editor.invisibles_enabled', $value );
 
-		$this->assertSame( $value, $this->user->get( 'editor.invisibles_enabled' ) );
+		$this->assertSame( $value, $this->user->get( $this->user_id, 'editor.invisibles_enabled' ) );
 	}
 
 	public function test_should_retrieve_editor_tabs_enabled() {
-		$this->assertSame( $this->dummy['editor']['tabs_enabled'], $this->user->get( 'editor.tabs_enabled' ) );
+		$this->assertSame( $this->dummy['editor']['tabs_enabled'], $this->user->get( $this->user_id, 'editor.tabs_enabled' ) );
 	}
 
 	public function test_should_update_editor_tabs_enabled() {
 		$value = 'off';
 
-		$this->user->set( 'editor.tabs_enabled', $value );
+		$this->user->set( $this->user_id, 'editor.tabs_enabled', $value );
 
-		$this->assertSame( $value, $this->user->get( 'editor.tabs_enabled' ) );
+		$this->assertSame( $value, $this->user->get( $this->user_id, 'editor.tabs_enabled' ) );
 	}
 
 	public function test_should_retrieve_editor_indent_width() {
-		$this->assertSame( $this->dummy['editor']['indent_width'], $this->user->get( 'editor.indent_width' ) );
+		$this->assertSame( $this->dummy['editor']['indent_width'], $this->user->get( $this->user_id, 'editor.indent_width' ) );
 	}
 
 	public function test_should_update_editor_indent_width() {
 		$value = '8';
 
-		$this->user->set( 'editor.indent_width', $value );
+		$this->user->set( $this->user_id, 'editor.indent_width', $value );
 
-		$this->assertSame( $value, $this->user->get( 'editor.indent_width' ) );
+		$this->assertSame( $value, $this->user->get( $this->user_id, 'editor.indent_width' ) );
 	}
 
 	public function test_should_patch_editor() {
@@ -96,63 +95,63 @@ class UserTest extends TestCase {
 			),
 		);
 
-		$this->user->patch( $patch );
+		$this->user->patch( $this->user_id, $patch );
 
-		$this->assertSame( $patch['editor']['indent_width'], $this->user->get( 'editor.indent_width' ) );
-		$this->assertSame( $patch['editor']['tabs_enabled'], $this->user->get( 'editor.tabs_enabled' ) );
+		$this->assertSame( $patch['editor']['indent_width'], $this->user->get( $this->user_id, 'editor.indent_width' ) );
+		$this->assertSame( $patch['editor']['tabs_enabled'], $this->user->get( $this->user_id, 'editor.tabs_enabled' ) );
 	}
 
 	public function test_should_throw_exception_getting_unknown_option() {
 		$this->expectException( InvalidArgumentException::class );
 
-		$this->user->get( 'unknown_option' );
+		$this->user->get( $this->user_id, 'unknown_option' );
 
 		$this->expectException( InvalidArgumentException::class );
 
-		$this->user->get( 'editor.unknown_option' );
+		$this->user->get( $this->user_id, 'editor.unknown_option' );
 	}
 
 	public function test_should_throw_exception_setting_unknown_option() {
 		$this->expectException( InvalidArgumentException::class );
 
-		$this->user->set( 'unknown_option', 'test' );
+		$this->user->set( $this->user_id, 'unknown_option', 'test' );
 
 		$this->expectException( InvalidArgumentException::class );
 
-		$this->user->set( 'editor.unknown_option', 'test' );
+		$this->user->set( $this->user_id, 'editor.unknown_option', 'test' );
 	}
 
 	public function test_should_throw_exception_patching_unknown_option() {
 		$this->expectException( InvalidArgumentException::class );
 
-		$this->user->patch( array( 'unknown_option' => 'test' ) );
+		$this->user->patch( $this->user_id, array( 'unknown_option' => 'test' ) );
 
 		$this->expectException( InvalidArgumentException::class );
 
-		$this->user->patch( array( 'editor' => array( 'unknown_option' => 'test' ) ) );
+		$this->user->patch( $this->user_id, array( 'editor' => array( 'unknown_option' => 'test' ) ) );
 	}
 
 	public function test_should_fall_back_to_defaults() {
-		delete_user_meta( get_current_user_id(), 'wpgp_options' );
+		delete_user_meta( $this->user_id, 'wpgp_options' );
 
-		$this->assertEquals( $this->user->all(), array(
+		$this->assertEquals( $this->user->all( $this->user_id ), array(
 			'editor' => array(
 				'theme'              => 'default',
 				'invisibles_enabled' => 'off',
 				'tabs_enabled'       => 'off',
-				'indent_width'       => '4',
+				'indent_width'       => '2',
 			),
 		) );
 	}
 
 	public function test_should_read_from_legacy_values() {
-		delete_user_meta( get_current_user_id(), 'wpgp_options' );
+		delete_user_meta( $this->user_id, 'wpgp_options' );
 
 		foreach ( $this->legacy as $key => $value ) {
-			update_user_meta( get_current_user_id(), "_wpgp_{$key}", $value );
+			update_user_meta( $this->user_id, "_wpgp_{$key}", $value );
 		}
 
-		$this->assertSame( $this->user->all(), array(
+		$this->assertSame( $this->user->all( $this->user_id ), array(
 			'editor' => array(
 				'theme'              => 'test',
 				'invisibles_enabled' => 'on',
