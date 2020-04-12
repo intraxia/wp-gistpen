df129265db5cf3c24fa6b7d3051f16298aeb204b
James DiGioia <jamesorodig@gmail.com>
2020-04-12 15:01:43 -0400

    Add Shortcode component (#752)
    Renders the shortcode. Mostly useful for providing a JS API around
    what values the shortcode allows.

--- client/components/index.ts
+++ client/components/index.ts
@@ -2,3 +2,4 @@ export { default as Commits } from './Commits';
 export { default as EditPage } from './EditPage';
 export { default as SearchPopup } from './SearchPopup';
 export { default as SettingsPage } from './SettingsPage';
+export * from './Shortcode';
