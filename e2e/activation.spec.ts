4d6f32720d77e622bf68ccc1b28c5911e66d89b0
mAAdhaTTah <jamesorodig@gmail.com>
2020-04-11 20:51:36 -0400

    Create & register the block + add e2e tests
    Now that the block is completed, we can register that block and
    run our e2e tests against it to make sure it works.

--- e2e/activation.spec.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-env jest */
-import { activatePlugin } from '@wordpress/e2e-test-utils';
-
-describe('activation', () => {
-  it('should activate from the plugins page', async () => {
-    await activatePlugin('wp-gistpen');
-  });
-});
