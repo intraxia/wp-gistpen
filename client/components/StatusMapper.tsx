7677fc09fc3a28621debea95d59f6d1c47e97ed8
mAAdhaTTah <jamesorodig@gmail.com>
2020-04-12 21:22:48 -0400

    Extract StatusMapper component
    This makes it easier to map a set of statuses to the proper render
    function we need to create it. This pattern is going to be used
    across the application so let's get this in place early.

---
+++ client/components/StatusMapper.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+type Renderer = ReturnType<React.FC<any>>;
+
+type Props<T extends string> = {
+  status: T;
+  elements: {
+    [K in T]: () => React.ReactElement;
+  };
+};
+
+export const StatusMapper = <T extends string>({
+  status,
+  elements
+}: Props<T>): React.ReactElement => {
+  return elements[status]();
+};
