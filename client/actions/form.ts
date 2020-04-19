5cb340855a87134b91c1da097b74547ba7bbd788
James DiGioia <jamesorodig@gmail.com>
2020-04-19 11:08:09 -0400

    Add TextControl component from WP components (#779)
    This allows us to use the component with other brookjs components.

---
+++ client/actions/form.ts
@@ -0,0 +1,5 @@
+import { createAction } from 'typesafe-actions';
+
+export const change = createAction('CHANGE', resolve => (value: string) =>
+  resolve({ value })
+);
---
+++ client/components/TextControl.tsx
@@ -0,0 +1,10 @@
+import { toJunction } from 'brookjs';
+import { TextControl } from '@wordpress/components';
+import { Stream } from 'kefir';
+import { change } from '../actions';
+
+const events = {
+  onChange: (e$: Stream<string, never>) => e$.map(change)
+};
+
+export default toJunction(events)(TextControl);
