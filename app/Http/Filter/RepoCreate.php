208bc2a6b5d09a781ce0bd1ae241966d1973e1ba
James DiGioia <jamesorodig@gmail.com>
2020-05-17 11:50:13 -0400

    Extract BlobCreate filter & remove custom impl (#860)
    A lot of the rules we've implemented with custom methods can
    instead be handled by WordPress core. We can instead remove
    the methods and use the normal schema logic.
    
    We also extract BlobCreate to a separate filter and borrow its
    rules in the RepoCreate filter so we can reuse them across both
    the Repo creation controller and incoming Blob creation controller.
    
    Fix #614.

--- app/Http/Filter/RepoCreate.php
+++ app/Http/Filter/RepoCreate.php
@@ -11,6 +11,22 @@ use WP_Error;
  */
 class RepoCreate extends BaseFilter {
 	/**
+	 * BlobCreate filter.
+	 *
+	 * @var BlobCreate
+	 */
+	protected $blob;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param BlobCreate $blob
+	 */
+	public function __construct( BlobCreate $blob ) {
+		$this->blob = $blob;
+	}
+
+	/**
 	 * Generates argument rules.
 	 *
 	 * Returns an array matching the WP-API format for argument rules,
@@ -21,15 +37,16 @@ class RepoCreate extends BaseFilter {
 	public function rules() {
 		return [
 			'description' => [
-				'description'       => __( 'A description of the Repo.', 'wp-gistpen' ),
-				'required'          => true,
-				'sanitize_callback' => [ $this, 'sanitize_description' ],
+				'description' => __( 'A description of the Repo.', 'wp-gistpen' ),
+				'required'    => true,
+				'type'        => 'string',
 			],
 			'status'      => [
-				'description'       => __( 'Status of the Repo.', 'wp-gistpen' ),
-				'required'          => false,
-				'default'           => 'draft',
-				'sanitize_callback' => [ $this, 'sanitize_status' ],
+				'description' => __( 'Status of the Repo.', 'wp-gistpen' ),
+				'required'    => false,
+				'default'     => 'draft',
+				'type'        => 'string',
+				'enum'        => array_keys( get_post_statuses() ),
 			],
 			'password'    => [
 				'description' => __( 'Password for the Repo.', 'wp-gistpen' ),
@@ -37,135 +54,22 @@ class RepoCreate extends BaseFilter {
 				'default'     => '',
 			],
 			'sync'        => [
-				'description'       => __( 'Whether the Repo should be synced to Gist.', 'wp-gistpen' ),
-				'required'          => false,
-				'default'           => 'off',
-				'sanitize_callback' => [ $this, 'sanitize_sync' ],
+				'description' => __( 'Whether the Repo should be synced to Gist.', 'wp-gistpen' ),
+				'required'    => false,
+				'default'     => 'off',
+				'type'        => 'string',
+				'enum'        => [ 'on', 'off' ],
 			],
 			'blobs'       => [
-				'description'       => __( 'Individual code snippets attached to the repo.', 'wp-gistpen' ),
-				'required'          => false,
-				'default'           => [],
-				'sanitize_callback' => [ $this, 'sanitize_blobs' ],
+				'description' => __( 'Individual code snippets attached to the repo.', 'wp-gistpen' ),
+				'required'    => false,
+				'default'     => [],
+				'type'        => 'array',
+				'items'       => [
+					'type'       => 'object',
+					'properties' => $this->blob->rules(),
+				],
 			],
-		];
-	}
-
-	/**
-	 * Sanitize the description value.
-	 *
-	 * @param  string $description The description starting value.
-	 * @return string|WP_Error     Sanitized value, or error if invalid.
-	 */
-	public function sanitize_description( $description ) {
-		if ( ! is_string( $description ) ) {
-			return $this->create_error( __( 'Param "description" must be a string.', 'wp-gistpen' ) );
-		}
-
-		return $description;
-	}
-
-	/**
-	 * Santize the status value.
-	 *
-	 * @param  string $status  The status starting value.
-	 * @return string|WP_Error The status, or an error if invalid.
-	 */
-	public function sanitize_status( $status ) {
-		if ( ! in_array( $status, array_keys( get_post_statuses() ), true ) ) {
-			return $this->create_error( __( 'Param "status" must be a valid post status.', 'wp-gistpen' ) );
-		}
-
-		return $status;
-	}
-
-	/**
-	 * Santizie the sync value.
-	 *
-	 * @param  string $sync    The provided sync value.
-	 * @return string|WP_Error The sync value, or error if invalid.
-	 */
-	public function sanitize_sync( $sync ) {
-		if ( ! in_array( $sync, [ 'on', 'off' ], true ) ) {
-			return $this->create_error( __( 'Param "sync" must be one of: on, off.', 'wp-gistpen' ) );
-		}
-
-		return $sync;
-	}
-
-	/**
-	 * Ensure the blobs passed to the request are valid.
-	 *
-	 * @param  array $blobs   Blobs parameter.
-	 * @return WP_Errpr|array Sanitized blobs.
-	 */
-	public function sanitize_blobs( $blobs ) {
-		if ( ! is_array( $blobs ) ) {
-			return $this->create_error( __( 'Param "blob" must be an array.', 'wp-gistpen' ) );
-		}
-
-		$new_blobs = [];
-
-		foreach ( $blobs as $index => $blob ) {
-			$new_blob = $this->sanitize_blob( $blob, $index );
-
-			if ( is_wp_error( $new_blob ) ) {
-				return $new_blob;
-			}
-
-			$new_blobs[] = $new_blob;
-		}
-
-		return $blobs;
-	}
-
-	/**
-	 * Ensures the individual blob passed to blobs is valid.
-	 *
-	 * @param  array $blob Blob to sanitize.
-	 * @param  int   $index Current loop index.
-	 * @return WP_Error|array       Sanitized blob.
-	 */
-	public function sanitize_blob( $blob, $index ) {
-		if ( ! is_array( $blob ) ) {
-			return $this->create_error( sprintf(
-				/* translators: %d: Blob index. */
-				__( 'Param "blob[%d]" must be an object.', 'wp-gistpen' ),
-				$index
-			) );
-		}
-
-		if (
-			// Make sure it's set
-			! isset( $blob['filename'] ) ||
-			// is a string
-			! is_string( $blob['filename'] ) ||
-			// and is not empty.
-			'' === $blob['filename']
-			// @todo simplyify logic?
-		) {
-			return $this->create_error( sprintf(
-				/* translators: %d: Blob index. */
-				__( 'Param "blob[%d].filename" must be a non-empty string.', 'wp-gistpen' ),
-				$index
-			) );
-		}
-
-		if (
-			! isset( $blob['code'] ) ||
-			! is_string( $blob['code'] )
-		) {
-			return $this->create_error( sprintf(
-				/* translators: %d: Blob index. */
-				__( 'Param "blob[%d].code" must be a string.', 'wp-gistpen' ),
-				$index
-			) );
-		}
-
-		return [
-			'filename' => $blob['filename'],
-			'code'     => $blob['code'],
-			'language' => isset( $blob['language'] ) ? $blob['language'] : null,
 		];
 	}
 }
