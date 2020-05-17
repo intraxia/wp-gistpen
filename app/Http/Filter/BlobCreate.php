a171816cbcd41d3cf99cafa915234fd325313d18
mAAdhaTTah <jamesorodig@gmail.com>
2020-05-17 11:41:33 -0400

    Extract BlobCreate filter & remove custom impl
    A lot of the rules we've implemented with custom methods can
    instead be handled by WordPress core. We can instead remove
    the methods and use the normal schema logic.
    
    We also extract BlobCreate to a separate filter and borrow its
    rules in the RepoCreate filter so we can reuse them across both
    the Repo creation controller and incoming Blob creation controller.
    
    Fix #614.

---
+++ app/Http/Filter/BlobCreate.php
@@ -0,0 +1,72 @@
+<?php
+namespace Intraxia\Gistpen\Http\Filter;
+
+use Intraxia\Jaxion\Core\Config;
+use Intraxia\Gistpen\Model\Language;
+use WP_Error;
+
+/**
+ * Class Filter\BlobCreate
+ *
+ * @package Intraxia\Gistpen\Http
+ * @subpackage Filters
+ */
+class BlobCreate extends BaseFilter {
+	/**
+	 * Constructor.
+	 *
+	 * @param Config $config
+	 */
+	public function __construct( Config $config ) {
+		$this->config = $config;
+	}
+
+	/**
+	 * Generates argument rules.
+	 *
+	 * Returns an array matching the WP-API format for argument rules,
+	 * including sanitization, validation, required, or defaults.
+	 *
+	 * @return array
+	 */
+	public function rules() {
+		$languages = $this->config->get_json_resource( 'languages' );
+
+		return [
+			'filename' => [
+				'description'       => __( 'Blob filename.', 'wp-gistpen' ),
+				'required'          => true,
+				'type'              => 'string',
+				'sanitize_callback' => [ $this, 'sanitize_filename' ],
+			],
+			'code'     => [
+				'description' => __( 'Code for the blob.', 'wp-gistpen' ),
+				'required'    => true,
+				'type'        => 'string',
+			],
+			'language' => [
+				'description' => __( 'Language the blob code is in.', 'wp-gistpen' ),
+				'type'        => 'string',
+				'enum'        => array_merge(
+					array_keys( $languages['list'] ),
+					array_keys( $languages['aliases'] )
+				),
+			],
+		];
+	}
+
+	/**
+	 * Sanitize the Blob's filename.
+	 *
+	 * @param string $filename
+	 */
+	public function sanitize_filename( $filename ) {
+		if ( '' === $filename ) {
+			return $this->create_error( sprintf(
+				__( 'Param "filename" must be a non-empty string.', 'wp-gistpen' )
+			) );
+		}
+
+		return $filename;
+	}
+}
