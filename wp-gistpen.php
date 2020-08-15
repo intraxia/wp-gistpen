0d397c853aacfff8fcc902743135fc189e9576b4
mAAdhaTTah <jamesorodig@gmail.com>
2020-08-15 16:15:52 -0400

    2.0.0-alpha.0

--- wp-gistpen.php
+++ wp-gistpen.php
@@ -14,7 +14,7 @@
  * Plugin Name:       WP-Gistpen
  * Plugin URI:        http://www.jamesdigioia.com/wp-gistpen/
  * Description:       A self-hosted alternative to putting your code snippets on Gist.
- * Version:           1.2.1
+ * Version:           2.0.0-alpha.0
  * Author:            James DiGioia
  * Author URI:        http://www.jamesdigioia.com/
  * Text Domain:       wp-gistpen
